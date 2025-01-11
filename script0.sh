25fcdd17585f0b19beb6b949ec61d6610d2f5cf8
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-11 18:24:42 +0530

    Update script0.sh

--- script0.sh
+++ script0.sh
@@ -1,44 +1,21 @@
 #!/bin/sh
 
-# Last amended: 10th Jan, 2025
-# To execute this file, you need to 
-# execute the following lines in Ubuntu console
-# Note the multiline bash comment here.
-: '
-echo "Downloading files from Internet"
+# Last amended: 11th Jan, 2025
+
+
+echo "Downloading files from Internet
+
 wget -c https://raw.githubusercontent.com/harnalashok/LLMs/refs/heads/main/script1.sh
 wget -c https://raw.githubusercontent.com/harnalashok/LLMs/refs/heads/main/script2.sh
 wget -c https://raw.githubusercontent.com/harnalashok/LLMs/refs/heads/main/script3.sh
+echo "Done....."
+sleep 2
 
 perl -pi -e 's/\r\n/\n/g' script1.sh
 perl -pi -e 's/\r\n/\n/g' script2.sh
 perl -pi -e 's/\r\n/\n/g' script3.sh
 
 chmod +x *.sh
+
+echo " Changing host name"
 bash script1.sh
-'
-
-
-# Install software
-sudo apt update
-sudo apt upgrade -y
-sudo apt install net-tools cmake build-essential -y 
-
-# Install uv for langflow install
-echo "Installing uv"
-curl -LsSf https://astral.sh/uv/install.sh | sh
-
-
-# Change machine name
-echo "Changing machine name..."
-echo '[boot]' | sudo tee  /etc/wsl.conf > /dev/null
-echo 'systemd=true' | sudo tee -a /etc/wsl.conf > /dev/null
-echo '[network]' | sudo tee -a /etc/wsl.conf > /dev/null
-echo 'hostname = master' | sudo tee -a /etc/wsl.conf > /dev/null
-echo 'generateHosts = false' | sudo tee -a /etc/wsl.conf > /dev/null
-sudo rm /etc/resolv.conf
-echo 'nameserver 8.8.8.8' | sudo tee  /etc/resolv.conf > /dev/null
-sudo sed -i 's/127.0.1.1.*/127.0.1.1  master.fsm.ac.in   master/' /etc/hosts
-
-# Shut down ubuntu
-wsl.exe --shutdown
