3257dea52347a6017160310f35b7f34cc236f9fe
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-11 19:01:01 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -5,24 +5,43 @@
 # Last amended: 10th Jan, 2025
 
 # Install software
+echo " "
+echo "Installing llama.cpp"
+echo "------------"
+echo " "
 git clone https://github.com/ggerganov/llama.cpp
 cd llama.cpp
 cmake -B build
 cmake --build build --config Release
 cd ~
 echo "PATH=\$PATH:/home/ashok/llama.cpp/build/bin" >> .bashrc
-
+echo " "
+echo "-------"
+echo "Done......"
+echo "-------"
 sleep 3
 
 # Downloading one gguf model
-echo "Downloading gguf model. Takes time...."
+echo "  "
+echo "Downloading gguf model from huggingface"
+echo "Will take time....."
+echo "-------------------"
+echo " "
 cd ~/llama.cpp/models
 wget -c   https://huggingface.co/prithivMLmods/Llama-Thinker-3B-Preview-GGUF/resolve/main/llama-thinker-3b-preview-q8_0.gguf?download=true
 # You may have to issue the following command to cleanup also.
 mv 'llama-thinker-3b-preview-q8_0.gguf?download=true' llama-thinker-3b-preview-q8_0.
 sleep 3
+echo " "
+echo "Done......"
+echo "---------"
 
-echo "Shutting down Ubuntu"
+echo "  "
+echo "Will shut down Ubuntu console"
+echo "After shutdown, reopen ubuntu console and execute the command:"
+echo "    ./script3.sh"
+echo "----------"
+echo " "
 sleep 4
-wsl.exe --shutdown
+wsl.exe  --shutdown
 
