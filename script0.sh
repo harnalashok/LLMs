beb94a19e018954f065206fb0e6321647e627a86
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-11 18:28:15 +0530

    Update script0.sh

--- script0.sh
+++ script0.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 
 # Last amended: 11th Jan, 2025
+# Execute this file as:
+#      ./script0.sh
+#  OR
+@      bash script0.sh
 
 
-echo "Downloading files from Internet
-
+echo "Downloading script files from Internet
+echo "---------------------------"
+echo " "
+wget -c https://raw.githubusercontent.com/harnalashok/LLMs/refs/heads/main/script0.sh
 wget -c https://raw.githubusercontent.com/harnalashok/LLMs/refs/heads/main/script1.sh
 wget -c https://raw.githubusercontent.com/harnalashok/LLMs/refs/heads/main/script2.sh
 wget -c https://raw.githubusercontent.com/harnalashok/LLMs/refs/heads/main/script3.sh
+echo "  "
 echo "Done....."
-sleep 2
+sleep 4
 
+# Doc to Unix conversion
+perl -pi -e 's/\r\n/\n/g' script0.sh
 perl -pi -e 's/\r\n/\n/g' script1.sh
 perl -pi -e 's/\r\n/\n/g' script2.sh
 perl -pi -e 's/\r\n/\n/g' script3.sh
