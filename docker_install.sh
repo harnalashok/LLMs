a1976f2bc9acedd241991fe569537f26cf70a385
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-12 10:20:20 +0530

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -34,7 +34,7 @@ echo "Installing docker repo and its Certificate"
 cd ~
 apt install ca-certificates curl gnupg lsb-release  -y
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -f -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 echo "Updating Ubuntu packages list.."
