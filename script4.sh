3bae7950310823f0e995ff7fba2482853b482632
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-12 12:16:56 +0530

    Update script4.sh

--- script4.sh
+++ script4.sh
@@ -8,6 +8,23 @@
 #     script4.sh
 #     docker_install.sh
 
+# Downloading a larger gguf model
+echo "  "
+echo "Will download one gguf model from huggingface"
+echo "Will take lot of time....."
+echo "-------------------"
+echo " "
+sleep 9
+cd ~/llama.cpp/models
+wget -c   https://huggingface.co/prithivMLmods/Llama-Thinker-3B-Preview-GGUF/resolve/main/llama-thinker-3b-preview-q8_0.gguf?download=true
+# You may have to issue the following command to cleanup also.
+mv 'llama-thinker-3b-preview-q8_0.gguf?download=true' llama-thinker-3b-preview-q8_0.
+
+echo " "
+echo "Done......"
+echo "---------"
+sleep 5
+
 
 # Test ollama and tinyllama
 ollama run tinyllama
