eb56b2ccc8f3c62695015d91d7b308a15a22e622
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-12 17:10:46 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -11,11 +11,16 @@
 #     docker_install.sh
 #     test.sh
 
-# Test ollama and tinyllama
-ollama run tinyllama
+# Download ollama nomic-embed-text
+echo " Pulling text-embedding model"
+ollama pull nomic-embed-text
+sleep 4
+echo " Pulling olomo2 model"
+ollama pull olmo2
+sleep 5
+
 # Test langfloe
 uv run langflow --version
-# uv run langflow --help
 
 # 2.2 Test Flowise:
 npx flowise start
@@ -26,3 +31,5 @@ sudo docker run hello-world
 # Test llama.cpp
 cd /home/ashok/llama.cpp/models
 llama-cli -m gemma-2-2b-it.Q6_K.gguf -p "I believe the meaning of life is" -n 128
+
+mv test.sh /home/ashok/done
