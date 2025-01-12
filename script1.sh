09dced32e9f0baeb26633d2a718ca6a5dce345e0
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-12 10:50:20 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -26,26 +26,7 @@ echo "------------------"
 sleep 9
 sudo curl -fsSL https://fnm.vercel.app/install | bash
 
-# Docker Engine install
-echo " "
-echo "Will install docker engine..."
-echo "------------------"
-sleep 9
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-echo " "
-echo " "
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Change machine name
 echo " "
