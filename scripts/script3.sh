6ec8034c26bb44a5cf175fe896da0ab8c656ce2b
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-12 19:00:22 +0530

    Add files via upload

---
+++ scripts/script3.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+# These sscripts run in sequence.
+#     script0.sh
+#     script1.sh
+#     script2.sh
+#     script3.sh
+#     script4.sh
+#     docker_install.sh
+#     test.sh
+
+
+# Install langflow
+echo " "
+echo "Installing langflow..."
+echo "------"
+echo " "
+sleep 9
+uv venv
+uv pip install langflow 
+sleep 4
+
+# 2.1 Install Flowise as NORMAL user
+echo " "
+echo "Installing flowvise...Takes time..."
+echo "------"
+echo " "
+sleep 9
+npm install -g flowise
+
+
+
+# Download tinyllama
+echo " "
+echo "--------- "
+echo "Downloading tinyllama"
+echo "Download size is 637MB"
+echo "------"
+echo " "
+ollama pull tinyllama
+
+# Move script file to done folder
+mv /home/ashok/script3.sh /home/ashok/done
+mv /home/ashok/next/docker_install.sh  /home/ashok
+
+echo " "
+echo "Will shut down Ubuntu console"
+echo "Reopen it and install docker, as:"
+echo "  sudo ./docker_install.sh"
+echo "----------"
+echo " "
+sleep 10
+wsl.exe --shutdown
+
+
