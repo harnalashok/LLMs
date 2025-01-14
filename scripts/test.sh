10a62ad83873e19673de5ab6444281d85569df23
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-14 17:31:28 +0530

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -22,11 +22,14 @@ echo " " | tee -a error.log
 
 
 echo "Installing milvus vector database using docker"    | tee -a error.log
+echo "You will be asked for the password. Supply it..."    | tee -a error.log
 echo "It is assumed that docker engine is already installed."    | tee -a error.log
 echo " "    | tee -a error.log
 sleep 9
+
 curl -sfL https://raw.githubusercontent.com/milvus-io/milvus/master/scripts/standalone_embed.sh -o standalone_embed.sh
 bash standalone_embed.sh start  2>> error.log
+
 echo " "
 echo "Milvus installed"    | tee -a error.log
 echo "To stop docker use the following commands:"
@@ -44,6 +47,7 @@ echo "Starting ollama in background"    | tee -a error.log
 echo "---------"    | tee -a error.log
 echo " "    | tee -a error.log
 ollama serve &  > /dev/null &
+
 echo " "
 echo " Pulling text-embedding model"    | tee -a error.log
 echo " "    | tee -a error.log
