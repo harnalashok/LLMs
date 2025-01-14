8a3c9a85b3778a6a767a033fa7f2b2cb5548e2e1
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-14 15:34:09 +0530

    Update script2.sh

--- scripts/script2.sh
+++ scripts/script2.sh
@@ -33,18 +33,18 @@ fi
 echo " "
 echo " "
 echo "------"
-echo "Installing ollama. Take time...."
+echo "Installing ollama. Takes time...."  | tee -a error.log
 echo "When asked, supply password"
 echo "------"
 echo " "
 echo " "
-curl -fsSL https://ollama.com/install.sh | sh
+curl -fsSL https://ollama.com/install.sh | sh  2>> error.log
 sleep 9
 
 
 # Installing llama.cpp
 echo " "
-echo "Installing llama.cpp"
+echo "Installing llama.cpp"  | tee -a error.log
 echo "------------"
 echo " "
 git clone https://github.com/ggerganov/llama.cpp
@@ -61,26 +61,26 @@ echo "-------"
 # 1.2 download and install Node.js
 echo " "
 echo "-------"
-echo "Installing Node.js......"
+echo "Installing Node.js ver 20......"  | tee -a error.log
 echo "-------"
-fnm use --install-if-missing 20
+fnm use --install-if-missing 20   2>> error.log
 echo " "
 
 
 # Downloading smaller gguf model
 echo "  "
-echo "Will download gemma-2 gguf model from huggingface"
+echo "Will download gemma-2 gguf model from huggingface"  | tee -a error.log
 echo "Will take lot of time....."
 echo "-------------------"
 echo " "
 sleep 9
 cd ~/llama.cpp/models
-wget -c   https://huggingface.co/MaziyarPanahi/gemma-2-2b-it-GGUF/resolve/main/gemma-2-2b-it.Q6_K.gguf?
+wget -c   https://huggingface.co/MaziyarPanahi/gemma-2-2b-it-GGUF/resolve/main/gemma-2-2b-it.Q6_K.gguf? 2>> error.log
 # You may have to issue the following command to cleanup also.
 mv 'gemma-2-2b-it.Q6_K.gguf?' gemma-2-2b-it.Q6_K.gguf
 
 echo " "
-echo "gemma-2-2b-it.Q6_K.gguf downloaded"
+echo "gemma-2-2b-it.Q6_K.gguf downloaded"  | tee -a error.log
 echo "---------"
 sleep 9
 
