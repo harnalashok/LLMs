34ec9d389e865a62aeda57c5fb395e0f429be206
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-15 10:15:06 +0530

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,10 +11,8 @@
 #     script3.sh
 #     script4.sh
 #     docker_install.sh
+#     model_install.sh
 #     test.sh
-
-# Milvus install
-# Ref: https://milvus.io/docs/install_standalone-docker.md
 
 
 echo " " | tee -a error.log
@@ -24,26 +22,6 @@ echo "**********" | tee -a error.log
 echo " " | tee -a error.log
 
 
-
-# Download ollama nomic-embed-text
-# Start ollama in background
-echo " "    | tee -a error.log
-echo "-----------"    | tee -a error.log
-echo "Starting ollama in background"    | tee -a error.log
-echo "---------"    | tee -a error.log
-echo " "    | tee -a error.log
-ollama serve &  > /dev/null &
-
-echo " "
-echo " Pulling text-embedding model"    | tee -a error.log
-echo " "    | tee -a error.log
-echo "--------- "    | tee -a error.log
-
-ollama pull nomic-embed-text  2>> error.log
-sleep 6
-echo "Pulling olomo2 model"    | tee -a error.log
-ollama pull olmo2  2>> error.log
-sleep 5
 
 # Test langflow
 echo " "    | tee -a error.log
