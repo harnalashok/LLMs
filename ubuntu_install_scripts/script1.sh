fc018838655fa39f742084ac7a4e432807e924e4
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-19 07:45:41 +0530

    Update script1.sh

--- ubuntu_install_scripts/script1.sh
+++ ubuntu_install_scripts/script1.sh
@@ -109,10 +109,6 @@ if [[ `hostname` != "master" ]]; then
    sudo rm /etc/resolv.conf
    echo 'nameserver 8.8.8.8' | sudo tee  /etc/resolv.conf > /dev/null
    sudo sed -i 's/127.0.1.1.*/127.0.1.1  master.fsm.ac.in   master/' /etc/hosts
-else
-   echo "It appears you have already executed this script."
-   echo "Recheck.....and press ctrl+c  if yes."
-   sleep 40
 fi
 
 # Install uv for langflow install
