4fba3f41a2246fe299d9b60aa30441f71793c54d
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-20 07:06:23 +0530

    Update script1.sh

--- ubuntu_install_scripts/script1.sh
+++ ubuntu_install_scripts/script1.sh
@@ -40,7 +40,7 @@ sudo apt install zip unzip net-tools cmake  build-essential python3-pip tilde cu
 
 echo " "
 echo "Ubuntu upgraded ......"  | tee -a ~/error.log
-echo "Ubuntu upgraded ......"  | tee -a ~/info.log
+echo "1. Ubuntu upgraded ......"  | tee -a ~/info.log
 
 # Install 'fnm' (Fast Node Manager)
 echo " "   | tee -a ~/error.log
@@ -49,7 +49,7 @@ echo "------------------"   | tee -a ~/error.log
 sleep 9
 sudo curl -fsSL https://fnm.vercel.app/install | bash   2>> ~/error.log
 echo "Fast Node Manager (fnm) installed"  | tee -a ~/error.log
-echo "Fast Node Manager (fnm) installed"  | tee -a ~/info.log
+echo "2. Fast Node Manager (fnm) installed"  | tee -a ~/info.log
 
 
 # Install chromadb
@@ -63,8 +63,8 @@ pip install chromadb   2>> ~/error.log
 sleep 2
 echo " "    | tee -a ~/error.log
 echo "ChromaDB installed"    | tee -a ~/error.log
-echo "ChromaDB installed"    | tee -a ~/info.log
-echo "Database is at: ~/.local/bin/chroma"  | tee -a ~/info.log
+echo "3. ChromaDB installed"    | tee -a ~/info.log
+echo "4. Database is at: ~/.local/bin/chroma"  | tee -a ~/info.log
 eecho "------ "    | tee -a ~/error.log
 
 #  TO START CHROMA AS a SERVICE
@@ -87,13 +87,13 @@ sudo mv chroma.service /etc/systemd/system/chroma.service
 
 # You can now start chroma, as:
 echo " "     | tee -a ~/error.log
-echo "---Start/Stop Chroma as-------"     | tee -a ~/info.log
-echo "sudo systemctl daemon-reload"   | tee -a ~/info.log
-echo "sudo systemctl enable chroma"     | tee -a ~/info.log
-echo "sudo systemctl start chroma"     | tee -a ~/info.log
-echo "Chroma is available at port 8000"     | tee -a ~/info.log
-echo "Check as: "      | tee -a ~/info.log
-echo "    netstat -aunt | grep 8000"      | tee -a ~/info.log
+echo "5. ---Start/Stop Chroma as-------"     | tee -a ~/info.log
+echo "     sudo systemctl daemon-reload"   | tee -a ~/info.log
+echo "     sudo systemctl enable chroma"     | tee -a ~/info.log
+echo "     sudo systemctl start chroma"     | tee -a ~/info.log
+echo "6. Chroma is available at port 8000"     | tee -a ~/info.log
+echo "7. Check as: "      | tee -a ~/info.log
+echo "      netstat -aunt | grep 8000"      | tee -a ~/info.log
 echo "----------"      | tee -a ~/info.log
 echo " "     | tee -a ~/info.log
 sleep 9
@@ -125,7 +125,7 @@ curl -LsSf https://astral.sh/uv/install.sh | sh   2>> error.log
 echo " "     | tee -a ~/error.log
 echo " "     | tee -a ~/info.log
 echo "uv installed"     | tee -a ~/error.log
-echo "uv installed"     | tee -a ~/info.log
+echo "8. uv installed"     | tee -a ~/info.log
 sleep 9
 
 # Move script file to done folder
