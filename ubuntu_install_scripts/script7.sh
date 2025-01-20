4dafcb44211feeae6f647e27a6021a50252195b0
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-20 07:46:26 +0530

    Update script7.sh

--- ubuntu_install_scripts/script7.sh
+++ ubuntu_install_scripts/script7.sh
@@ -11,66 +11,79 @@
 #     script6.sh
 #     script7.sh
 
-echo " " | tee -a error.log
-echo "*********"  | tee -a error.log
-echo "Script: script7.sh"  | tee -a error.log
-echo "**********" | tee -a error.log
-echo " " | tee -a error.log
+echo " " | tee -a ~/error.log
+echo "*********"  | tee -a ~/error.log
+echo "Script: script7.sh"  | tee -a ~/error.log
+echo "**********" | tee -a ~/error.log
+echo " " | tee -a ~/error.log
 
 
 # Download ollama nomic-embed-text
 if netstat -aunt   |  grep '11434'; then  
-   echo " "      | tee -a error.log
-   echo "Ollama is already started"     | tee -a error.log
-   echo " "      | tee -a error.log
+   echo " "      | tee -a ~/error.log
+   echo "Ollama is already started"     | tee -a ~/error.log
+   echo " "      | tee -a ~/error.log
  else  
     # Download ollama nomic-embed-text
     # Start ollama in background
-    echo " "    | tee -a error.log
-    echo "-----------"    | tee -a error.log
-    echo "Starting ollama in background"    | tee -a error.log
-    echo "---------"    | tee -a error.log
-    echo " "    | tee -a error.log
+    echo " "    | tee -a ~/error.log
+    echo "-----------"    | tee -a ~/error.log
+    echo "Starting ollama in background"    | tee -a ~/error.log
+    echo "---------"    | tee -a ~/error.log
+    echo " "    | tee -a ~/error.log
     ollama serve &  > /dev/null &
+	echo "You may also start ollama, as: "     | tee -a ~/info.log
+	echo "    ollama serve &  > /dev/null & "     | tee -a ~/info.log
+	
 fi
 
-echo " "    | tee -a error.log
-echo " Pulling text-embedding model for ollama"    | tee -a error.log
-echo " "    | tee -a error.log
-echo "--------- "    | tee -a error.log
+echo " "    | tee -a ~/error.log
+echo " Pulling text-embedding model for ollama"    | tee -a ~/error.log
+echo " "    | tee -a ~/error.log
+echo "--------- "    | tee -a ~/error.log
 
-ollama pull nomic-embed-text  2>> error.log
+ollama pull nomic-embed-text  2>> ~/error.log
 sleep 2
-echo "nomic-embed-text pulled"   | tee -a error.log
-echo "------- "    | tee -a error.log
-echo " "    | tee -a error.log
+echo "nomic-embed-text pulled"   | tee -a ~/error.log
+echo "------- "    | tee -a ~/error.log
+echo "nomic-embed-text pulled"   | tee -a ~/info.log
+echo "------- "    | tee -a ~/info.log
+echo " "    | tee -a ~/info.log
 
-echo " "    | tee -a error.log
-echo "Will pull olom2 model, next. File size: 4.5gb"  | tee -a error.log
+echo " "    | tee -a ~/error.log
+echo "Will pull olom2 model, next. File size: 4.5gb"  | tee -a ~/error.log
 read -p "Shall I go ahead and pull olom2 model (yY/nN? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    echo " "    | tee -a error.log
-    echo "OK. Pulling olomo2 model"    | tee -a error.log
-    ollama pull olmo2  2>> error.log
-sleep 5
+    echo " "    | tee -a ~/error.log
+    echo "OK. Pulling olomo2 model"    | tee -a ~/error.log
+    ollama pull olmo2  2>> ~/error.log
+	sleep 2
+	echo "olmo2  pulled"   | tee -a ~/info.log
+    echo "------- "    | tee -a ~/info.log
+    echo " "    | tee -a ~/info.log
+	sleep 5
 fi
 
 # Move script file to done folder
 mv ~/script7.sh  ~/done
 
-echo "Will test llama-cpp-python now"  | tee -a error.log
-echo  "*********"  | tee -a error.log
-echo " "  | tee -a error.log
-echo "Testing llama-cpp-python with Llama-2-13B-chat-GGUF" | tee -a error.log
-echo "Access it at localhost:8000/docs"  | tee -a error.log
-echo  "*********"  | tee -a error.log
+echo "Will test llama-cpp-python now"  | tee -a ~/error.log
+echo  "*********"  | tee -a ~/error.log
+echo " "  | tee -a ~/error.log
+echo "Testing llama-cpp-python with Llama-2-13B-chat-GGUF" | tee -a ~/error.log
+echo "Test llama-cpp-python, as: "  | tee -a ~/info.log
+echo "     llama-cpp-python with Llama-2-13B-chat-GGUF" | tee -a ~/info.log
+echo "Access it at localhost:8000/docs"  | tee -a ~/info.log
+echo  "*********"  | tee -a ~/error.log
 
 sleep 9
 
 python3 -m llama_cpp.server --model ~/llama.cpp/models//llama-2-13b-chat.Q4_K_M.gguf --host 0.0.0.0 --port 8000 --chat functionary & 
-echo " "  | tee -a error.log
+echo "python3 -m llama_cpp.server --model ~/llama.cpp/models//llama-2-13b-chat.Q4_K_M.gguf --host 0.0.0.0 --port 8000 --chat functionary & "   | tee -a ~/info.log
+echo " "  | tee -a ~/error.log
+echo " "  | tee -a ~/info.log
 sleep 9
 
 << ////
