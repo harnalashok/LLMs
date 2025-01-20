6334673b9ebd39056e271a859c1e20a07b55b43d
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-20 07:27:33 +0530

    Update script3.sh

--- ubuntu_install_scripts/script3.sh
+++ ubuntu_install_scripts/script3.sh
@@ -15,67 +15,75 @@
 #     script7.sh
 
 
-echo " " | tee -a error.log
-echo "*********"  | tee -a error.log
-echo "Script: script3.sh"  | tee -a error.log
-echo "**********" | tee -a error.log
-echo " " | tee -a error.log
+echo " " | tee -a ~/error.log
+echo "*********"  | tee -a ~/error.log
+echo "Script: script3.sh"  | tee -a ~/error.log
+echo "**********" | tee -a ~/error.log
+echo " " | tee -a ~/error.log
 
 
 # Install langflow
-echo " "   | tee -a error.log
-echo "Installing langflow..."  | tee -a error.log
-echo "------"   | tee -a error.log
-echo " "   | tee -a error.log
+echo " "   | tee -a ~/error.log
+echo "Installing langflow..."  | tee -a ~/error.log
+echo "------"   | tee -a ~/error.log
+echo " "   | tee -a ~/error.log
 sleep 9
 uv venv
-uv pip install langflow  2>> error.log
+uv pip install langflow  2>> ~/error.log
 sleep 2
-echo "  "    | tee -a error.log
-echo "  "    | tee -a error.log
-echo "langflow installed"   | tee -a error.log
+echo "  "    | tee -a ~/error.log
+echo "  "    | tee -a ~/info.log
+
+echo "langflow installed"   | tee -a ~/error.log
+echo "langflow installed"   | tee -a ~/info.log
+
+
 # https://docs.langflow.org/configuration-cli
-echo "Ref: https://docs.langflow.org/configuration-cli"      | tee -a error.log
-echo "Run following command to get langflow CLI options:"    | tee -a error.log
-echo "        uv run langflow"    | tee -a error.log
-echo "Generate api-key, as: "    | tee -a error.log
-echo "        uv run langflow api-key"    | tee -a error.log
-echo "Run langflow, as:"    | tee -a error.log
-echo "        uv run langflow run"    | tee -a error.log
-echo "---------- "   | tee -a error.log
-echo "  "   | tee -a error.log
+echo "Ref: https://docs.langflow.org/configuration-cli"      | tee -a ~/info.log
+echo "Run following command to get langflow CLI options:"    | tee -a ~/info.log
+echo "        uv run langflow"    | tee -a ~/info.log
+echo "Generate api-key, as: "    | tee -a ~/info.log
+echo "        uv run langflow api-key"    | tee -a ~/info.log
+echo "Run langflow, as:"    | tee -a ~/info.log
+echo "        uv run langflow run"    | tee -a ~/info.log
+echo "---------- "   | tee -a ~/info.log
+echo "  "   | tee -a ~/info.log
 
 sleep 9
 
 # 2.1 Install Flowise as NORMAL user
 echo " "
-echo "Installing flowvise...Takes time..."  | tee -a error.log
-echo "------"   | tee -a error.log
-echo " "   | tee -a error.log
+echo "Installing flowvise...Takes time..."  | tee -a ~/error.log
+echo "------"   | tee -a ~/error.log
+echo " "   | tee -a ~/error.log
 sleep 9
-npm install -g flowise  2>> error.log
+npm install -g flowise  2>> ~/error.log
 echo " "
 echo " "     | tee -a ~/error.log
 echo "flowise installed"    | tee -a ~/error.log
 echo " "     | tee -a ~/error.log
+echo "flowise installed"    | tee -a ~/info.log
+echo " "     | tee -a ~/info.log
 
 
 
 # Download tinyllama
-echo " "   | tee -a error.log
-echo "--------- "   | tee -a error.log
-echo "Downloading tinyllama for ollama"   | tee -a error.log
+echo " "   | tee -a ~/error.log
+echo "--------- "   | tee -a ~/error.log
+echo "Downloading tinyllama for ollama"   | tee -a ~/error.log
 echo "Download size is 637MB"
-echo "------"   | tee -a error.log
-echo " "   | tee -a error.log
-ollama pull tinyllama   2>> error.log
+echo "------"   | tee -a ~/error.log
+echo " "   | tee -a ~/error.log
+ollama pull tinyllama   2>> ~/error.log
 sleep 9
 
 echo "  "     | tee -a ~/error.log
 echo "  "     | tee -a ~/error.log
 echo "tinyllama downloaded"     | tee -a ~/error.log
-echo "Check as: ollama list"    | tee -a ~/error.log
-echo "-------"   | tee -a ~/error.log
+echo "tinyllama downloaded"     | tee -a ~/info.log
+
+echo "Check as: ollama list"    | tee -a ~/info.log
+echo "-------"   | tee -a ~/info.log
 
 # Move script file to done folder
 mv ~/script3.sh ~/done
