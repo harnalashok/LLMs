4c468cace92089090fafcce11ef60da49e7ce423
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-20 07:31:44 +0530

    Update script4.sh

--- ubuntu_install_scripts/script4.sh
+++ ubuntu_install_scripts/script4.sh
@@ -15,18 +15,18 @@
 #     script7.sh
 
 
-echo " " | tee -a error.log
-echo "*********"  | tee -a error.log
-echo "Script: script4.sh"  | tee -a error.log
-echo "**********" | tee -a error.log
-echo " " | tee -a error.log
+echo " " | tee -a ~/error.log
+echo "*********"  | tee -a ~/error.log
+echo "Script: script4.sh"  | tee -a ~/error.log
+echo "**********" | tee -a ~/error.log
+echo " " | tee -a ~/error.log
 
 
 # Check if Docker installed
 if docker -v  |  grep 'version'; then  
    echo " "
 else
-   echo "Docker engine is not installed. Install it first"   | tee -a error.log
+   echo "Docker engine is not installed. Install it first"   | tee -a ~/error.log
    sleep 10
    exit
 fi
@@ -35,34 +35,36 @@ fi
 # Milvus install
 # Ref: https://milvus.io/docs/install_standalone-docker.md
 
-echo "Installing milvus vector database using docker"    | tee -a error.log
-echo "You will be asked for the password. Supply it..."    | tee -a error.log
-echo "Ports used are: 9091 and 19530."    | tee -a ~/error.log
-echo " "    | tee -a error.log
+echo "Installing milvus vector database using docker"    | tee -a ~/error.log
+echo "You will be asked for the password. Supply it..."    | tee -a ~/error.log
+
+echo " "    | tee -a ~/error.log
 sleep 3
 
 curl -sfL https://raw.githubusercontent.com/milvus-io/milvus/master/scripts/standalone_embed.sh -o standalone_embed.sh
-bash standalone_embed.sh start  2>> error.log
+bash standalone_embed.sh start  2>> ~/error.log
 
 echo " "
-echo "Milvus vector database installed"    | tee -a error.log
-echo "To stop docker use the following commands:"
-echo "      bash standalone_embed.sh stop"
-echo "To delete the database, use the following command:"
-echo "      bash standalone_embed.sh delete"
-echo "--------------------"
+echo "Milvus vector database installed"    | tee -a ~/error.log
+echo "Milvus vector database installed"    | tee -a ~/info.log
+echo "Ports used are: 9091 and 19530."    | tee -a ~/info.log
+echo "To stop docker use the following commands:"    | tee -a ~/info.log
+echo "      bash standalone_embed.sh stop"    | tee -a ~/info.log
+echo "To delete the database, use the following command:"    | tee -a ~/info.log
+echo "      bash standalone_embed.sh delete"     | tee -a ~/info.log
+echo "--------------------"    | tee -a ~/info.log
 sleep 9
 
 
 
 # Downloading a larger gguf model
-echo "  "    | tee -a error.log
-echo "Will download a large gguf model from huggingface"  | tee -a error.log
-echo "Will take lot of time....."    | tee -a error.log
-echo "If broken, this download can be resumed as: "      | tee -a error.log
-echo "wget -c   https://huggingface.co/prithivMLmods/Llama-Thinker-3B-Preview-GGUF/resolve/main/llama-thinker-3b-preview-q8_0.gguf?download=true "  | tee -a error.log        
-echo "-------------------"    | tee -a error.log
-echo " "    | tee -a error.log
+echo "  "    | tee -a ~/error.log
+echo "Will download a large gguf model from huggingface"  | tee -a ~/error.log
+echo "Will take lot of time....."    | tee -a ~/error.log
+echo "If broken, this download can be resumed as: "      | tee -a ~/error.log
+echo "wget -c   https://huggingface.co/prithivMLmods/Llama-Thinker-3B-Preview-GGUF/resolve/main/llama-thinker-3b-preview-q8_0.gguf?download=true "  | tee -a ~/error.log        
+echo "-------------------"    | tee -a ~/error.log
+echo " "    | tee -a ~/error.log
 sleep 9
 cd ~/llama.cpp/models
 wget -c   https://huggingface.co/prithivMLmods/Llama-Thinker-3B-Preview-GGUF/resolve/main/llama-thinker-3b-preview-q8_0.gguf?download=true
@@ -72,10 +74,11 @@ wget -c   https://huggingface.co/prithivMLmods/Llama-Thinker-3B-Preview-GGUF/res
 # You may have to issue the following command to cleanup also.
 mv 'llama-thinker-3b-preview-q8_0.gguf?download=true' llama-thinker-3b-preview-q8_0.gguf
 
-echo " "    | tee -a error.log
-echo "thinker-3b-preview-q8_0.gguf downloaded"    | tee -a error.log
-echo "Check as: ls -la ~/llama.cpp/models/ "  | tee -a error.log
-echo "---------"    | tee -a error.log
+echo " "    | tee -a ~/error.log
+echo "thinker-3b-preview-q8_0.gguf downloaded"    | tee -a ~/error.log
+echo "thinker-3b-preview-q8_0.gguf downloaded"    | tee -a ~/info.log
+echo "Check as: ls -la ~/llama.cpp/models/ "  | tee -a ~/info.log
+echo "---------"    | tee -a ~/info.log
 sleep 9
 
 # Move script file to done folder
