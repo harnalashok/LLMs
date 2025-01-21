b484ed28aa8e4ae6016c32be92dff06dbf48a7ee
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-21 19:13:49 +0530

    Update script0.sh

--- ubuntu_install_scripts/script0.sh
+++ ubuntu_install_scripts/script0.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Last amended: 14th Jan, 2025
+# Last amended: 21st Jan, 2025
 
 # Connected scripts are:
 # These sscripts run in sequence.
@@ -91,22 +91,32 @@ chmod +x *.sh
 chmod +x ~/next/*.sh
 
 # Install Anaconda
+echo " "
+echo "Downloading Anaconda" | tee -a ~/error.log
 wget -c https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
 bash Anaconda3-2024.10-1-Linux-x86_64.sh -b
 sleep 2
+echo "Anaconda installed" | tee -a ~/info.log
 rm Anaconda3-2024.10-1-Linux-x86_64.sh
 echo "PATH=\$PATH:~/anaconda3/bin/" >> ~/.bashrc
-echo "conda init "  >> ~/info.log
-echo "conda activate " >> ~/info.log
-echo "conda deactivate " >> ~/info.log
 
+echo " "  | tee -a ~/info.log
+echo " ---------" | tee -a ~/error.log
+echo "To begin using Anaconda, you need to initialise it, as"  | tee -a ~/info.log
+echo "      conda init "  >> | tee -a ~/info.log
+echo "      Bring base environment,  as: conda activate " >> ~/info.log
+echo "      Remove base environment, as:  conda deactivate " >> ~/info.log
+echo "------"  | tee -a ~/info.log
+echo " "  | tee -a ~/info.log
 
 
 # Move script file to 'done' folder
 mv ~/script0.sh  ~/done
 # Bring in the next file
-mv ~/next/script1.sh  ~/
-echo "Terminal will close."
-echo "Open it again, and issue the following command"
-echo "
+mv ~/next/script1.sh  ~/  | tee -a ~/error.log
+
+echo "Terminal will close."  | tee -a ~/error.log
+echo "Open it again, and issue the following command:"   | tee -a ~/error.log
+echo "   ./script1.sh"  | tee -a ~/error.log
+exec sleep 9
 
