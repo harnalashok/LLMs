c677b64339f09dbcbb4d835a0b1916208e821ce5
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-22 15:26:23 +0530

    Update script8.sh

--- ubuntu_install_scripts/script8.sh
+++ ubuntu_install_scripts/script8.sh
@@ -11,19 +11,19 @@ sleep 9
 
 # Install Anaconda
 echo " "
-echo "Downloading Anaconda" | tee -a ~/error.log
+echo "Downloading Anaconda" | tee -a /home/ashok/error.log
 wget -c https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
 bash Anaconda3-2024.10-1-Linux-x86_64.sh -b
 sleep 2
-echo "Anaconda installed" | tee -a ~/info.log
+echo "Anaconda installed" | tee -a /home/ashok/info.log
 rm Anaconda3-2024.10-1-Linux-x86_64.sh
-echo "PATH=\$PATH:~/anaconda3/bin/" >> ~/.bashrc
+echo "PATH=\$PATH:/home/ashok/anaconda3/bin/" >> /home/ashok/.bashrc
 
-echo " "  | tee -a ~/info.log
-echo " ---------" | tee -a ~/error.log
-echo "To begin using Anaconda, you need to initialise it, as"  | tee -a ~/info.log
-echo "      conda init "  | tee -a ~/info.log
-echo "      Bring base environment,  as: conda activate " >> ~/info.log
-echo "      Remove base environment, as:  conda deactivate " >> ~/info.log
-echo "------"  | tee -a ~/info.log
-echo " "  | tee -a ~/info.log
+echo " "  | tee -a /home/ashok/info.log
+echo " ---------" | tee -a /home/ashok/error.log
+echo "To begin using Anaconda, you need to initialise it, as"  | tee -a /home/ashok/info.log
+echo "      conda init "  | tee -a /home/ashok/info.log
+echo "      Bring base environment,  as: conda activate " >> /home/ashok/info.log
+echo "      Remove base environment, as:  conda deactivate " >> /home/ashok/info.log
+echo "------"  | tee -a /home/ashok/info.log
+echo " "  | tee -a /home/ashok/info.log
