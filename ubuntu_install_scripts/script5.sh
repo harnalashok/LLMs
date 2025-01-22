11205ba267e5f406085ab62f68f1d2d497ed18b6
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-22 15:30:52 +0530

    Update script5.sh

--- ubuntu_install_scripts/script5.sh
+++ ubuntu_install_scripts/script5.sh
@@ -16,70 +16,70 @@
 #     script6.sh
 #     script7.sh
 
-echo " " | tee -a ~/error.log
-echo "*********"  | tee -a ~/error.log
-echo "Script: script5.sh"  | tee -a ~/error.log
-echo "**********" | tee -a ~/error.log
-echo " " | tee -a ~/error.log
+echo " " | tee -a /home/ashok/error.log
+echo "*********"  | tee -a /home/ashok/error.log
+echo "Script: script5.sh"  | tee -a /home/ashok/error.log
+echo "**********" | tee -a /home/ashok/error.log
+echo " " | tee -a /home/ashok/error.log
 
 #conda deactivate
 
 # Check ollama models
-echo "Ollama models are stored here"  | tee -a ~/info.log
-echo "-------------"  | tee -a ~/info.log
-ls -la  /usr/share/ollama/.ollama/models/blobs   | tee -a ~/info.log
-echo " "   | tee -a ~/info.log
+echo "Ollama models are stored here"  | tee -a /home/ashok/info.log
+echo "-------------"  | tee -a /home/ashok/info.log
+ls -la  /usr/share/ollama/.ollama/models/blobs   | tee -a /home/ashok/info.log
+echo " "   | tee -a /home/ashok/info.log
  
 
 # Stop ollama, if already started
 sudo systemctl stop ollama
 
 # Test langflow
-echo " "    | tee -a ~/error.log
-echo "Testing langflow"    | tee -a ~/error.log
-echo "---------"    | tee -a ~/error.log
-echo " "    | tee -a ~/error.log
-uv run langflow --version   2>> ~/error.log
+echo " "    | tee -a /home/ashok/error.log
+echo "Testing langflow"    | tee -a /home/ashok/error.log
+echo "---------"    | tee -a /home/ashok/error.log
+echo " "    | tee -a /home/ashok/error.log
+uv run langflow --version   2>> /home/ashok/error.log
 
 # 2.2 Test Flowise:
-echo " "    | tee -a ~/error.log
-echo "Starting flowise. Acess it at port 3000"    | tee -a ~/error.log
-echo "------- "    | tee -a ~/error.log
-echo " "    | tee -a ~/error.log
+echo " "    | tee -a /home/ashok/error.log
+echo "Starting flowise. Acess it at port 3000"    | tee -a /home/ashok/error.log
+echo "------- "    | tee -a /home/ashok/error.log
+echo " "    | tee -a /home/ashok/error.log
 sleep 9
-echo "Start flowise as: npx flowise start "    | tee -a ~/error.log
+echo "Start flowise as: npx flowise start "    | tee -a /home/ashok/error.log
 npx flowise start & > /dev/null &
 
 # Test docker
-echo " "    | tee -a ~/error.log
-echo "Installing image hello-world of docker"    | tee -a ~/error.log
-echo "------- "    | tee -a ~/error.log
-echo " "    | tee -a ~/error.log
+echo " "    | tee -a /home/ashok/error.log
+echo "Installing image hello-world of docker"    | tee -a /home/ashok/error.log
+echo "------- "    | tee -a /home/ashok/error.log
+echo " "    | tee -a /home/ashok/error.log
 sleep 9
-sudo docker run hello-world   2>> ~/error.log
+sudo docker run hello-world   2>> /home/ashok/error.log
 
 # Test llama.cpp
-echo " "    | tee -a ~/error.log
-echo "Testing llama.cpp"    | tee -a ~/error.log
-echo "------- "    | tee -a ~/error.log
-echo " "    | tee -a ~/error.log
+echo " "    | tee -a /home/ashok/error.log
+echo "Testing llama.cpp"    | tee -a /home/ashok/error.log
+echo "------- "    | tee -a /home/ashok/error.log
+echo " "    | tee -a /home/ashok/error.log
 
 # Move scripts
-mv ~/script5.sh  ~/done
-mv ~/next/script6.sh  ~/
+mv /home/ashok/script5.sh  /home/ashok/done
+mv /home/ashok/next/script6.sh  /home/ashok/
 
-echo " "     | tee -a ~/error.log
-echo "-------"     | tee -a ~/error.log
-echo "Sending a prompt to gemma-2-2b"     | tee -a ~/error.log
-echo "Press ctrl+c to stop it any time."    | tee -a ~/error.log
-echo "-------"     | tee -a ~/error.log
-echo " "     | tee -a ~/error.log
-echo " "     | tee -a ~/error.log
-echo "Test llama.cpp, as"    | tee -a ~/info.log
-echo "    llama-cli -m gemma-2-2b-it.Q6_K.gguf -p "I believe the meaning of life is" -n 128 "     | tee -a ~/info.log
+echo " "     | tee -a /home/ashok/error.log
+echo "-------"     | tee -a /home/ashok/error.log
+echo "Sending a prompt to gemma-2-2b"     | tee -a /home/ashok/error.log
+echo "Press ctrl+c to stop it any time."    | tee -a /home/ashok/error.log
+echo "-------"     | tee -a /home/ashok/error.log
+echo " "     | tee -a /home/ashok/error.log
+echo " "     | tee -a /home/ashok/error.log
+echo "Test llama.cpp, as"    | tee -a /home/ashok/info.log
+echo "    llama-cli -m gemma-2-2b-it.Q6_K.gguf -p "I believe the meaning of life is" -n 128 "     | tee -a /home/ashok/info.log
 echo "Press ctrl+c to kill the process..."
 sleep 9
-cd ~/llama.cpp/models
+cd /home/ashok/llama.cpp/models
 llama-cli -m gemma-2-2b-it.Q6_K.gguf -p "I believe the meaning of life is" -n 128
 exec sleep 2
 
