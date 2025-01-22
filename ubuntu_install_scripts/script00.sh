969b773ebd4a341d21e636fb7c6fc2e71e7c8156
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-22 12:17:57 +0530

    Update script00.sh

--- ubuntu_install_scripts/script00.sh
+++ ubuntu_install_scripts/script00.sh
@@ -2,8 +2,8 @@ minal and #!/bin/sh
 
 conda init
 
-mv ¬/script00.sh ¬/done
-mv ¬/next/script1.sh  ¬/
+mv ~/script00.sh ¬/done
+mv ~/next/script1.sh  ~/
 
 echo "Will close terminal. "  
 echo "Next, open terminal and issue command"
