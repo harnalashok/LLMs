d09e27579f5ea9241deb35f70cd09422c25d9508
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-22 11:21:12 +0530

    Update script00.sh

--- ubuntu_install_scripts/script00.sh
+++ ubuntu_install_scripts/script00.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+minal and #!/bin/sh
 
 conda init
 
@@ -6,7 +6,7 @@ mv ¬/script00.sh ¬/done
 mv ¬/next/script1.sh  ¬/
 
 echo "Will close terminal. "  
-echo "Next, issue command"
+echo "Next, open terminal and issue command"
 echo "   ./script1.sh"
 
 exec sleep 2
