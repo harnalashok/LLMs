0fbda12e76ed031d675c6b5a9fbe20363177fe8a
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-22 10:26:32 +0530

    Update script0.sh

--- ubuntu_install_scripts/script0.sh
+++ ubuntu_install_scripts/script0.sh
@@ -103,7 +103,7 @@ echo "PATH=\$PATH:~/anaconda3/bin/" >> ~/.bashrc
 echo " "  | tee -a ~/info.log
 echo " ---------" | tee -a ~/error.log
 echo "To begin using Anaconda, you need to initialise it, as"  | tee -a ~/info.log
-echo "      conda init "  >> | tee -a ~/info.log
+echo "      conda init "  | tee -a ~/info.log
 echo "      Bring base environment,  as: conda activate " >> ~/info.log
 echo "      Remove base environment, as:  conda deactivate " >> ~/info.log
 echo "------"  | tee -a ~/info.log
