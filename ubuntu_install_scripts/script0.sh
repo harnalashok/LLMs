d591797c8b00692a1e0a6f35f171852bb27f886e
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-22 10:34:46 +0530

    Update script0.sh

--- ubuntu_install_scripts/script0.sh
+++ ubuntu_install_scripts/script0.sh
@@ -117,6 +117,6 @@ mv ~/next/script1.sh  ~/  | tee -a ~/error.log
 
 echo "Terminal will close."  | tee -a ~/error.log
 echo "Open it again, and issue the following command:"   | tee -a ~/error.log
-echo "   ./script1.sh"  | tee -a ~/error.log
+echo "   ./script00.sh"  | tee -a ~/error.log
 exec sleep 9
 
