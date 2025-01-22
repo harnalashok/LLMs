9d2ad7f783453ede507689905a640603c3103c9a
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-22 15:29:43 +0530

    Update script6.sh

--- ubuntu_install_scripts/script6.sh
+++ ubuntu_install_scripts/script6.sh
@@ -17,46 +17,46 @@
 #     script7.sh
 
 
-echo " " | tee -a ~/error.log
-echo "*********"  | tee -a ~/error.log
-echo "Script: script6.sh"  | tee -a ~/error.log
-echo "**********" | tee -a ~/error.log
-echo " " | tee -a ~/error.log
+echo " " | tee -a /home/ashok/error.log
+echo "*********"  | tee -a /home/ashok/error.log
+echo "Script: script6.sh"  | tee -a /home/ashok/error.log
+echo "**********" | tee -a /home/ashok/error.log
+echo " " | tee -a /home/ashok/error.log
 
 #conda  deactivate
 
 # Install required packages:
-echo "Installing dependencies " | tee -a ~/error.log
-echo "*********"  | tee -a ~/error.log
+echo "Installing dependencies " | tee -a /home/ashok/error.log
+echo "*********"  | tee -a /home/ashok/error.log
 sudo apt -y install python3-pip python3-dev python3-venv gcc g++ make jq 
-echo "Dependencies installed"  | tee -a ~/error.log
-echo " " | tee -a ~/error.log
+echo "Dependencies installed"  | tee -a /home/ashok/error.log
+echo " " | tee -a /home/ashok/error.log
 sleep 9
 
 # Login as a common user and prepare Python virtual environment 
 #   to install [llama-cpp-python].
-echo " "  | tee -a ~/error.log
-echo "Installing llama-cpp-python " | tee -a ~/error.log
-echo "*********"  | tee -a ~/error.log
+echo " "  | tee -a /home/ashok/error.log
+echo "Installing llama-cpp-python " | tee -a /home/ashok/error.log
+echo "*********"  | tee -a /home/ashok/error.log
 
 # Creating virtual environment
- python3 -m venv --system-site-packages ~/llama 
+ python3 -m venv --system-site-packages /home/ashok/llama 
  # Activating virtual envitronment
- source ~/llama/bin/activate 
+ source /home/ashok/llama/bin/activate 
  # Install [llama-cpp-python]. 
  pip3 install llama-cpp-python[server] 
- echo " "  | tee -a ~/error.log
- echo "Installation of  llama-cpp-python done" | tee -a ~/error.log
- echo "*********"  | tee -a ~/error.log
+ echo " "  | tee -a /home/ashok/error.log
+ echo "Installation of  llama-cpp-python done" | tee -a /home/ashok/error.log
+ echo "*********"  | tee -a /home/ashok/error.log
  
- echo "Installation of  llama-cpp-python done" | tee -a ~/info.log
- echo "Activate virtual environment as: source ~/llama/bin/activate "   | tee -a ~/info.log
- echo "*********"  | tee -a ~/info.log
+ echo "Installation of  llama-cpp-python done" | tee -a /home/ashok/info.log
+ echo "Activate virtual environment as: source /home/ashok/llama/bin/activate "   | tee -a /home/ashok/info.log
+ echo "*********"  | tee -a /home/ashok/info.log
  sleep 9
 
 # Move scripts
-mv script6.sh  ~/done/
-mv ~/next/script7.sh  ~/
+mv script6.sh  /home/ashok/done/
+mv /home/ashok/next/script7.sh  /home/ashok/
 
  
 # Downloading thellama-2-13b-chat.Q4_K_M.gguf format model.
@@ -66,25 +66,25 @@ mv ~/next/script7.sh  ~/
 #  ⇒ https://huggingface.co/TheBloke/Llama-2-13B-chat-GGUF/tree/main
 #  ⇒ https://huggingface.co/TheBloke/Llama-2-70B-Chat-GGUF/tree/main 
 
- echo " "  | tee -a ~/error.log
- echo "Downloading Llama-2-13B-chat-GGUF" | tee -a ~/error.log
- echo "Downloading size: 7.8GB" | tee -a ~/error.log
- echo "to folder ~/llama.cpp/models/"  | tee -a ~/error.log
- echo  "*********"  | tee -a ~/error.log
- cd  ~/llama.cpp/models/
+ echo " "  | tee -a /home/ashok/error.log
+ echo "Downloading Llama-2-13B-chat-GGUF" | tee -a /home/ashok/error.log
+ echo "Downloading size: 7.8GB" | tee -a /home/ashok/error.log
+ echo "to folder /home/ashok/llama.cpp/models/"  | tee -a /home/ashok/error.log
+ echo  "*********"  | tee -a /home/ashok/error.log
+ cd  /home/ashok/llama.cpp/models/
  wget https://huggingface.co/TheBloke/Llama-2-13B-chat-GGUF/resolve/main/llama-2-13b-chat.Q4_K_M.gguf 
  sleep 2
- echo " "  | tee -a ~/error.log
- echo "Downloaded Llama-2-13B-chat-GGUF" | tee -a ~/error.log
- echo "Downloaded Llama-2-13B-chat-GGUF" | tee -a ~/info.log
- echo "Downloaded size: 7.8GB" | tee -a ~/info.log
- echo "Folder is ~/llama.cpp/models/"  | tee -a ~/info.log
+ echo " "  | tee -a /home/ashok/error.log
+ echo "Downloaded Llama-2-13B-chat-GGUF" | tee -a /home/ashok/error.log
+ echo "Downloaded Llama-2-13B-chat-GGUF" | tee -a /home/ashok/info.log
+ echo "Downloaded size: 7.8GB" | tee -a /home/ashok/info.log
+ echo "Folder is /home/ashok/llama.cpp/models/"  | tee -a /home/ashok/info.log
  sleep 9
  
   
 # Move script file to done folder
-mv ~/script6.sh ~/done
-mv ~/next/script7.sh ~/
+mv /home/ashok/script6.sh /home/ashok/done
+mv /home/ashok/next/script7.sh /home/ashok/
 
 echo "  "
 echo "Will shut down Ubuntu console"
