03dfc3b821d79b8a9e34ddce0816868e30cdb21a
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-24 14:51:39 +0530

    Update script4.sh

--- u_install/script4.sh
+++ u_install/script4.sh
@@ -58,16 +58,16 @@ mkdir /home/ashok/milvus
 mv standalone_embed.sh /home/ashok/milvus/
 echo "PATH=$PATH:/home/ashok/milvus/" >> .bashrc
 
-echo "Ports are: 9091 and 19530."       >> /home/ashok/start_milvus.sh
-echo "cd /home/ashok/milvus"            >> /home/ashok/start_milvus.sh
-echo "bash standalone_embed.sh start"   >> /home/ashok/start_milvus.sh
+echo "echo 'Ports are: 9091 and 19530.'"       >> /home/ashok/start_milvus.sh
+echo "cd /home/ashok/milvus"                   >> /home/ashok/start_milvus.sh
+echo "bash standalone_embed.sh start"          >> /home/ashok/start_milvus.sh
 
-echo "Ports are: 9091 and 19530."       >> /home/ashok/stop_milvus.sh
-echo "cd /home/ashok/milvus"            >> /home/ashok/stop_milvus.sh
-echo "bash standalone_embed.sh stop"    >> /home/ashok/stop_milvus.sh
+echo "echo 'Ports are: 9091 and 19530.'"       >> /home/ashok/stop_milvus.sh
+echo "cd /home/ashok/milvus"                   >> /home/ashok/stop_milvus.sh
+echo "bash standalone_embed.sh stop"           >> /home/ashok/stop_milvus.sh
 
-echo "cd /home/ashok/milvus"            >> /home/ashok/delete_milvus_db.sh
-echo "bash standalone_embed.sh delete"  >> /home/ashok/delete_milvus_db.sh
+echo "cd /home/ashok/milvus"                   >> /home/ashok/delete_milvus_db.sh
+echo "bash standalone_embed.sh delete"         >> /home/ashok/delete_milvus_db.sh
 sleep 3
 
 # Install Flowise through docker"
@@ -85,7 +85,7 @@ echo "            docker stop flowise"                     | tee -a /home/ashok/
 echo " Also, check all containers available, as:"
 echo "             docker ps -a "                          | tee -a /home/ashok/info.log
 
-echo "Flowise port 3000 onstarting"                       >> /home/ashok/start_flowise.sh
+echo "echo 'Flowise port 3000 onstarting'"                >> /home/ashok/start_flowise.sh
 echo "cd /home/ashok/Flowise"                             >> /home/ashok/start_flowise.sh
 echo "docker start flowise"                               >> /home/ashok/start_flowise.sh
 echo "cd /home/ashok/Flowise"                             >> /home/ashok/stop_flowise.sh
@@ -100,11 +100,11 @@ cd langflow/docker_example
 sudo docker-compose up -d
 netstat -aunt | grep 7860
 
-echo "langflow port 7860 onstarting"                       >> /home/ashok/start_langflow.sh
+echo "echo 'langflow port 7860 onstarting'"                       >> /home/ashok/start_langflow.sh
 echo "cd /home/ashok/langflow/docker_example"              >> /home/ashok/start_langflow.sh
 echo "docker-compose up -d"                                   >> /home/ashok/start_langflow.sh
 
-echo "langflow port: 7860"                                 >> /home/ashok/stop_langflow.sh
+echo "echo 'langflow port: 7860'"                                 >> /home/ashok/stop_langflow.sh
 echo "cd /home/ashok/langflow/docker_example"              >> /home/ashok/stop_langflow.sh
 echo "docker-compose down"                                 >> /home/ashok/stop_langflow.sh
 
