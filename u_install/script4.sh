fa74e3f1df3812df6a539f3aa8cc2a48bf68065b
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-27 06:24:30 +0530

    Update script4.sh

--- u_install/script4.sh
+++ u_install/script4.sh
@@ -2,17 +2,14 @@
 
 # Last amended: 14th Jan, 2024
 
-# Connected scripts are:
-# These sscripts run in sequence.
-#     script0.sh
-#     script1.sh
-#     script2.sh
-#     script3.sh
-#     docker_install.sh
-#     script4.sh
-#     script5.sh
-#     script5.sh
-#     script7.sh
+ # These sscripts run in sequence.
+      #     script0.sh
+      #     script1.sh
+      #     script2.sh
+      #     docker_install.sh
+      #     script3.sh
+      #     script4.sh
+      #     script5.sh
 
 
 echo " "                                      | tee -a /home/ashok/error.log
