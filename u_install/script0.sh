2169d77208da76bfbbcef3d83591f04ecac80be9
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-27 06:22:12 +0530

    Update script0.sh

--- u_install/script0.sh
+++ u_install/script0.sh
@@ -2,18 +2,14 @@
 
 # Last amended: 23rd Jan, 2025
 
-# Connected scripts are:
-# These sscripts run in sequence.
-#     script0.sh
-#     script1.sh
-#     script2.sh
-#     script3.sh
-#     docker_install.sh
-#     script4.sh
-#     script5.sh
-#     script6.sh
-#     script7.sh
-#     script8.sh
+ # These scripts run in sequence.
+      #     script0.sh
+      #     script1.sh
+      #     script2.sh
+      #     docker_install.sh
+      #     script3.sh
+      #     script4.sh
+      #     script5.sh
 
 
 #------ Steps ---------
