f01308c13dcb1d22b56253339bdd349a13ea883d
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-27 17:39:01 +0530

    Create script6.sh

---
+++ u_install/script6.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install langchain and langgraph
+
+# Create python environment for installing
+python3 -m venv /home/ashok/langchain
+source /home/ashok/langchain/bin/activate
+pip install langchain
+pip install langchain-openai
+pip install langchain-community
+pip install langchain-experimental
+pip install langgraph
+pip install "langserve[all]"
+pip install langchain-cli
+deactivate
+
+sleep 2
