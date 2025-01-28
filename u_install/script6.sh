c21e70427828e5a3c4e62d4e1032fd0048581d6f
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-01-28 19:25:27 +0530

    Update script6.sh

--- u_install/script6.sh
+++ u_install/script6.sh
@@ -38,6 +38,34 @@ echo "echo 'Call as: source venv_langchain.sh' " > venv_langchain.sh
 echo "source /home/ashok/langchain/bin/activate"  >> venv_langchain.sh
 sleep 2
 
+# Install openwebui
+# Check existing python versions
+ls /usr/bin/python*
+
+# Install python 3.11
+sudo apt install python3.11 -y
+
+# Install tool to create python venv
+# invokable with its own Python executable
+sudo apt install python3.11-venv  -y
+
+# Check again python versions
+ls /usr/bin/python*
+
+# Create python virtual env at openwebui
+# Using python3.11 package
+
+python3.11 -m venv /home/ashok/openwebui
+# Activate the python env
+source /home/ashok/openwebui/bin/activate
+# Install openwebui. Takes time
+pip3.11 install open-webui
+
+# Start open-webui once
+open-webui serve
+
+
+
 # Move scripts
 mv /home/ashok/script6.sh  /home/ashok/done/
 mv /home/ashok/next/download_models.sh   /home/ashok/
