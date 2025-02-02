e3435a64c2987c8981b403a634bd6613c7a6669f
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-02-02 20:26:07 +0530

    Update script7.sh

--- u_install/script7.sh
+++ u_install/script7.sh
@@ -12,12 +12,16 @@ sleep 10
 
 cd ~/
 
+##########################
+### Install FAISS library
+##########################
+
 # Create venv for FAISS
 python3 -m venv /home/ashok/faiss
 source /home/ashok/faiss/bin/activate
 pip3 install faiss-cpu
 
-
+## Script to activate FAISS library
 echo '#!/bin/bash'                                                      > /home/ashok/start/activate_faiss.sh
 echo " "                                                                >> /home/ashok/start/activate_faiss.sh
 echo "cd ~/"                                                            >> /home/ashok/start/activate_faiss.sh
@@ -25,6 +29,10 @@ echo "echo 'Activate FAISS library, as:'"                                >> /hom
 echo "echo 'source /home/ashok/start/activate_faiss.sh'"                 >> /home/ashok/start/activate_faiss.sh
 echo "echo 'To deactivate issue just the command: deactivate'"           >> /home/ashok/start/activate_faiss.sh
 echo "source /home/ashok/faiss/bin/activate"                             >> /home/ashok/start/activate_faiss.sh
+
+##########################
+### Install LMStudio
+##########################
 
 # Install LM Studio
 # sudo apt install libnss3-dev libgdk-pixbuf2.0-dev libgtk-3-dev libxss-dev
@@ -45,44 +53,41 @@ echo "./LM-Studio-0.3.9-3-x64.AppImage"                  >> /home/ashok/start/st
 cp /home/ashok/start/start_lmstudio.sh  /home/ashok/lms/
 chmod +x /home/ashok/lms/*.sh
 
-## Script to develop a symlink for any gguf folder in
-#  in ~/llama.cpp/models folder to ~/.lmstudio/models
+## Script to develop a symlink for any gguf file in
+#  in ~/llama.cpp/models folder (target is:  ~/.lmstudio/models)
 
 echo '#!/bin/bash'                                       > /home/ashok/lms/symlink_gguf.sh
 echo " "                                                 >> /home/ashok/lms/symlink_gguf.sh
-
 echo "if [ "\$\#" -ne 2 ]"                               >> /home/ashok/lms/symlink_gguf.sh
 echo "then"                                              >> /home/ashok/lms/symlink_gguf.sh
-   echo "echo 'Incorrect number of arguments'"           >> /home/ashok/lms/symlink_gguf.sh
+   echo "echo 'Incorrect number of arguments.'"           >> /home/ashok/lms/symlink_gguf.sh
+   echo "echo '  '"                                       >> /home/ashok/lms/symlink_gguf.sh
    echo "echo 'Usage: ./symlink_gguf.sh YourModelName  ggufFileName'"  >> /home/ashok/lms/symlink_gguf.sh
    echo "echo 'Example:'"                                      >> /home/ashok/lms/symlink_gguf.sh
    echo "echo './symlink_gguf gemma gemma-2-2b-it.Q6_K.gguf'"  >> /home/ashok/lms/symlink_gguf.sh
-   echo "echo 'File gemma-2-2b-it.Q6_K.gguf must be in ~/llama.cpp/models folder'"   >> /home/ashok/lms/symlink_gguf.sh
+   echo "echo '    (file gemma-2-2b-it.Q6_K.gguf must be in ~/llama.cpp/models folder)'"   >> /home/ashok/lms/symlink_gguf.sh
    echo "exit 1"                                         >> /home/ashok/lms/symlink_gguf.sh
 echo "fi"                                                >> /home/ashok/lms/symlink_gguf.sh
-
-echo "echo 'Start LMStudio and change Models directory to: /home/ashok/.lmstudio'"  >> /home/ashok/lms/symlink_gguf.sh
+echo "echo 'After starting LMStudio, change Models directory to:'"  >> /home/ashok/lms/symlink_gguf.sh
+echo "echo '    /home/ashok/.lmstudio to list the gguf file'"       >> /home/ashok/lms/symlink_gguf.sh
 echo "sleep 9"                                           >> /home/ashok/lms/symlink_gguf.sh
 echo "mkdir ~/.lmstudio/models/\$1"                       >> /home/ashok/lms/symlink_gguf.sh
 # ii) Move to your home folder
 echo "cd ~/"                                             >> /home/ashok/lms/symlink_gguf.sh
 # iii) Create a softlink of a gguf file in the current folder:
 echo "ln -s ~/llama.cpp/models/\$2"                       >> /home/ashok/lms/symlink_gguf.sh
-      
 # iv) Move this symlink to flder created above:
 echo "mv \$2 /home/ashok/.lmstudio/models/\$1"             >> /home/ashok/lms/symlink_gguf.sh
 chmod +x /home/ashok/lms/*.sh
+cp ~/lms/symlink.sh  ~/
 
 
-
-
-
-
-
-
-# Install Anything LLM
+##########################
+### Install AnythingLLM
+##########################
 curl -fsSL https://cdn.useanything.com/latest/installer.sh | sh
 
+# Script to start anythingLLM
 echo '#!/bin/bash'                                                      > /home/ashok/start/start_anythingllm.sh
 echo " "                                                                >> /home/ashok/start/start_anythingllm.sh
 echo "cd ~/"                                                            >> /home/ashok/start/start_anythingllm.sh
@@ -90,6 +95,7 @@ echo "cd /home/ashok/AnythingLLMDesktop/anythingllm-desktop"           >> /home/
 echo "./anythingllm-desktop start"                                     >>  /home/ashok/start/start_anythingllm.sh
 
 
+# Misc 
 chmod +x /home/ashok/*.sh
 chmod +x /home/ashok/start/*.sh
 chmod +x /home/ashok/stop/*.sh
