7df88113297114fca3b09c8f31c8d86771d1ee72
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-02-04 13:24:21 +0530

    Update get_chroma_collectionsName.py

--- u_install/get_chroma_collectionsName.py
+++ u_install/get_chroma_collectionsName.py
@@ -1,3 +1,7 @@
+# python program to get names of collections
+# # Use as:
+#         python3 get_chroma_collectionsName.py
+
 import chromadb
 client = chromadb.PersistentClient(path="/home/ashok/Documents/data/")
 collections = client.list_collections()
