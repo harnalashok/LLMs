df5fdcaafd193dac9b9d3a2a44d07fbfc71a1f21
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-02-04 13:27:14 +0530

    Update script8.sh

--- u_install/script8.sh
+++ u_install/script8.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+ # These scripts run in sequence.
+      #     script0.sh
+      #     script1.sh
+      #     script2.sh
+      #     docker_install.sh
+      #     script3.sh
+      #     script4.sh
+      #     script5.sh
+      #     script6.sh
+      #     script7.sh
+      #     script8.sh
+
+
+
 echo "========script7=============="
 echo "Will create script to create postgresql user/database"  
 echo "Needed for RecordManager"
