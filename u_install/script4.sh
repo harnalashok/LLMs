c913640864154c1e2c45a74fff80bee87d84d0a7
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-02-05 15:43:59 +0530

    Update script4.sh

--- u_install/script4.sh
+++ u_install/script4.sh
@@ -168,7 +168,7 @@ docker volume create portainer_data
 sudo docker run -d -p 8888:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.5
 comment
 
-sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.5
+sudo docker run -d -p 8888:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.5
 cd ~/
 # Script to start portainer container
 
