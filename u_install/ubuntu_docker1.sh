d481e20322a9ea7838a05d8d13854e23eb5c3bc8
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-02-06 19:01:40 +0530

    Update ubuntu_docker1.sh

--- u_install/ubuntu_docker1.sh
+++ u_install/ubuntu_docker1.sh
@@ -8,7 +8,8 @@
       #     script0.sh
       #     script1.sh
       #     script2.sh
-      #     ubuntu_docker.sh
+      #     ubuntu_docker1.sh
+      #     ubuntu_docker2.sh
       #     script3.sh
       #     script4.sh
       #     script5.sh
@@ -33,16 +34,15 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Check if docker installed
-sudo docker run hello-world
 
-# Run docker witout root privileges
-sudo groupadd docker
-sudo usermod -aG docker $USER
+# Move script file to done folder
+mv /home/ashok/ubuntu_docker1.sh       /home/ashok/done
+mv /home/ashok/next/ubuntu_docker2.sh  /home/ashok/
 
-sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
-sudo chmod g+rwx "$HOME/.docker" -R
-
-#sudo systemctl disable docker.service
-#sudo systemctl disable containerd.service
+echo "Machine will be rebooted "
+echo "After restart, execute:"
+echo "    ./ubuntu_docker2.sh"
+sleep 10
 reboot
+
+
