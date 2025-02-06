ecec3b6aa22150a189feda93910faa3d75dcce6a
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-02-06 19:20:02 +0530

    Update script4.sh

--- u_install/script4.sh
+++ u_install/script4.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+# Last amended: 27th Jan, 2024
+# These sscripts run in sequence.
+      #     script0.sh
+      #     script1.sh
+      #     script2.sh
+      #     docker_install.sh
+      #     script3.sh
+      #     script4.sh
+      #     script5.sh
+      #     script6.sh
+
+
+mkdir /home/ashok/localai
+docker run -ti --name local-ai -p 8080:8080 localai/localai:latest-cpu
