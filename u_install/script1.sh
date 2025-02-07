29640b6c49eeaae89fa597a4b2abb11184d231b9
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-02-07 06:15:56 +0530

    Update script1.sh

--- u_install/script1.sh
+++ u_install/script1.sh
@@ -50,7 +50,7 @@ echo " "
 sleep 2
 sudo apt update
 sudo apt upgrade -y
-sudo apt install zip unzip net-tools cmake  build-essential python3-pip tilde curl git  python3-dev python3-venv gcc g++ make jq  libfuse2 -y  
+sudo apt install zip unzip net-tools cmake  build-essential python3-pip tilde curl git  python3-dev python3-venv gcc g++ make jq  openssh-server libfuse2 -y  
 sudo apt -y install python3-pip python3-dev python3-venv gcc g++ make jq 
 echo " "
 echo "Ubuntu upgraded ......"                | tee -a /home/ashok/error.log
