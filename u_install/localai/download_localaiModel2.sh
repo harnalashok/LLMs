189a4ecdb385a74b42b032f4616a7635d88e0ba5
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-02-07 13:23:05 +0530

    Add files via upload

---
+++ u_install/localai/download_localaiModel2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Download localai model--III
+# Better download it directly from localai models gallery
+
+
+LOCALAI=http://localhost:8080
+curl $LOCALAI/models/apply -H "Content-Type: application/json" -d '{
+     "id": "LocalAI-functioncall-phi-4-v0.3"
+   }'
+
