fc9dcbd9336e86f5942be6654772918e4eb09afe
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-02-08 09:26:12 +0530

    Update ubuntu_docker1.sh

--- u_install/ubuntu_docker1.sh
+++ u_install/ubuntu_docker1.sh
@@ -30,7 +30,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt-get update -y
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
