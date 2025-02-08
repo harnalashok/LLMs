ffacd96e9f22e183dd30e20d93f1af7a8e2e8182
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-02-08 14:36:50 +0530

    Update download_status.sh

--- u_install/localai/download_status.sh
+++ u_install/localai/download_status.sh
@@ -2,8 +2,10 @@
 
 # Ref: https://localai.io/models/
 
+echo " "
 echo "Download status to localai repo, only if download is in progress."
 echo "Else, status may give error message"
+echo " "
 
 curl http://localhost:8080/models/jobs/
 
