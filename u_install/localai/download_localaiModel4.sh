6174fc3dbf9cba6d4e8fff49dd815d84a2630352
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-02-12 20:05:56 +0530

    Add files via upload

---
+++ u_install/localai/download_localaiModel4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Download localai model--IV
+# Better download it directly from localai models gallery
+
+
+LOCALAI=http://localhost:8080
+curl $LOCALAI/models/apply -H "Content-Type: application/json" -d '{
+     "id": "mistral-7b-instruct-v0.3"
+   }'
+
