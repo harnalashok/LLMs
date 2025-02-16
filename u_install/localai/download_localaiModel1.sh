67b33f72cb20df9a095b2dc68ed98339b080b68c
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-02-16 12:39:44 +0530

    Update download_localaiModel1.sh

--- u_install/localai/download_localaiModel1.sh
+++ u_install/localai/download_localaiModel1.sh
@@ -2,9 +2,12 @@
 
 # Download localai model--I
 
-
 LOCALAI=http://localhost:8080
 curl $LOCALAI/models/apply -H "Content-Type: application/json" -d '{
      "id": "localai@bert-embeddings"
    }'
+   
+echo " "
+echo "To check download status, execute: ./get_download_status.sh "   
+echo " "
 
