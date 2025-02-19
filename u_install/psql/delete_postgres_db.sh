777f67cca00dd96726d55936a095e476f95552ae
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-02-19 12:51:40 +0530

    Update delete_postgres_db.sh

--- u_install/psql/delete_postgres_db.sh
+++ u_install/psql/delete_postgres_db.sh
@@ -11,6 +11,16 @@ echo "  "
 echo "Example command to drop database:"
 echo '          drop database gdatabase ;'
 echo " "
+echo "============= "
+echo "You can then, create database as:"
+echo '          CREATE DATABASE mydatabase WITH OWNER = myuser;'
+echo "============= "
+echo "Next, connect to your database, as: "
+echo "(This is akin to 'use db' command in mysql)"
+echo "           \c <databaseName>  "
+echo "Once connected, issue the following SQL command to make it vector store:"  
+echo "           CREATE EXTENSION vector; "
+echo "============="
 echo 'To quit psql shell enter \q'
 echo " "
 sleep 10
