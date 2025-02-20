39a5e680ad992b7e881b701309171c6be3906750
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-02-20 12:28:25 +0530

    Update createpostgresuser.sh

--- u_install/psql/createpostgresuser.sh
+++ u_install/psql/createpostgresuser.sh
@@ -10,15 +10,31 @@ echo " "
 echo 'Enter SQL command as in example below to create  a user LOGIN id'
 echo ' and his password, mypass (password be within single inverted commas)'
 echo "  "
+echo "AAA."
 echo "Example:  CREATE ROLE myuser LOGIN PASSWORD 'mypass' ; "
 echo " "
+echo "BBB."
 echo "Create database as:"
 echo '          CREATE DATABASE mydatabase WITH OWNER = myuser;'
 echo " "
+echo "++++++++++++++++++++"
+echo "CCC."
 echo "Within a database, you can create a schema, as:"
-echo '          CREATE schema myschema ; '
+echo '          a. First, connect to database:  \c mydatabase  '
+echo "          b. Then,  "
+echo "                    CREATE schema myschema ; "
+echo "(Every database has a default schema created by name of: public)"
 echo " "
-
+echo "DDD."
+echo "Finally create table, as:"
+echo "     CREATE TABLE mydatabase.myschema.distributors ( "
+echo "                                                   did  integer PRIMARY KEY,"
+echo "                                                   name    varchar(40), "
+echo "                                                   designation char(5)   "                          
+echo "                                                   ); "
+echo " "
+echo "+++++++++++++++++++++++++++"
+echo " "
 echo 'To quit psql shell enter \q'
 echo " "
 sleep 10
