976ccea69ad7ea54a4aab91a5aa9c7cc7ae554c8
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-03-02 18:42:28 +0530

    Update script7.sh

--- u_install/script7.sh
+++ u_install/script7.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 # Last amended: 27th Jan, 2024
-# These sscripts run in sequence.
-      #     script0.sh
-      #     script1.sh
-      #     script2.sh
-      #     docker_install.sh
-      #     script3.sh
-      #     script4.sh
-      #     script5.sh
-      #     script6.sh
-      #     script7.sh
-      #     script8.sh
-      #     script9.sh
-
 
       
 echo "========script7=============="
