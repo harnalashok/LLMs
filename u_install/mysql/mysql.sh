c118ce4fb1538490e3cc5627d47ac6e5f4dc24c4
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-03-02 15:58:45 +0530

    Update mysql.sh

--- u_install/mysql/mysql.sh
+++ u_install/mysql/mysql.sh
@@ -3,8 +3,9 @@
 echo " "
 echo "---------"
 echo "Logging in as root user"
-echo "When asked, supply 'sudo' password of ashok"
-echo "But 'root' has no password. When asked, press ENTER"
+echo "When asked, supply 'sudo' password of ashok, supply it"
+echo "But 'root' has no password. When asked, just press ENTER"
 echo "----------"
 echo " "
+sleep 10
 sudo mysql -u root -p
