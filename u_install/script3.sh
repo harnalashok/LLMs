fc521de0ce8629564e232f0bdb81db914e4dc215
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-03-02 18:39:56 +0530

    Update script3.sh

--- u_install/script3.sh
+++ u_install/script3.sh
@@ -2,20 +2,6 @@
 
 # LAst amended: 14th Jan, 2024
 
-# Connected scripts are:
-# These sscripts run in sequence.
-#     script0.sh
-#     script1.sh
-#     script2.sh
-#     ubuntu_docker1.sh
-#     ubuntu_docker2.sh
-#     script3.sh
-#     script4.sh
-#     script5.sh
-#     script6.sh
-#     script7.sh
-#     script8.sh
-
 cd ~/
 
 
