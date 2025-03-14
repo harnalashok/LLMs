c6e62386baf86e756118ec2105113a892ff07bae
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-03-14 18:17:54 +0530

    Update download_localaiModel1.sh

--- u_install/localai/download_localaiModel1.sh
+++ u_install/localai/download_localaiModel1.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # Download localai model--I
+echo " "
+echo "Actual model is: Llama-3.2-1B-Instruct-Q4_K_M-GGUF"
+echo "llama3.2 embeddings model"
+echo "Can be used as drop-in replacement for bert-embeddings" 
+echo " "
+sleep 4
 
 LOCALAI=http://localhost:8080
 curl $LOCALAI/models/apply -H "Content-Type: application/json" -d '{
