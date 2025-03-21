7546e89c0f09ca3fce879f1514b5511771870a44
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-03-21 13:40:18 +0530

    Update script4.sh

--- u_install/script4.sh
+++ u_install/script4.sh
@@ -54,11 +54,37 @@ mv /home/ashok/next/script5.sh  /home/ashok/
 
 mkdir /home/ashok/localai
 cd /home/ashok/localai
-# Should run in detached mode
-docker run -ti -d --name local-ai -p 8080:8080 localai/localai:latest-cpu
 
-echo "Download localai model"
-echo "Process will run in background"
+# PReparing docker for GPU
+# Ref StackOverflow: https://stackoverflow.com/a/77269071
+
+# 1.0 Configure the repository:
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey |sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+&& curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list \
+&& sudo apt-get update
+
+# 2.0 Install the NVIDIA Container Toolkit packages:
+
+sudo apt-get install -y nvidia-container-toolkit
+
+# 3.0  Configure the container runtime by using the nvidia-ctk command:
+
+sudo nvidia-ctk runtime configure --runtime=docker
+
+# 4.0 Restart the Docker daemon:
+
+sudo systemctl restart docker
+
+
+# Should run in detached mode
+#  docker run -ti -d --name local-ai -p 8080:8080 localai/localai:latest-cpu
+
+# Install localai using Nvidia GPU:
+# https://github.com/mudler/LocalAI
+docker run -ti --name local-ai -p 8080:8080 --gpus all localai/localai:latest-gpu-nvidia-cuda-12
+
+#echo "Download localai model"
+#echo "Process will run in background"
 sleep 5
 
 # Start local-ai in future
