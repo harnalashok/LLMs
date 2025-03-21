56ac121a7e6c9a8c7f254ac551e6208860d2fb09
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-03-21 07:02:04 +0530

    Add files via upload

---
+++ u_install/localai/search_docker_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Search docker data
+
+echo " "
+echo "==========="
+echo "Search data/models (for example localai models) under one of these folders"
+echo "==========="
+echo " "
+sudo  sudo ls -la /var/lib/docker/volumes
+
+echo " "
+echo " "
+
