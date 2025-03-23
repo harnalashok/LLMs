119b1a5f9b94a231c617d827aeb7a235b2fa29bf
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-03-23 07:45:39 +0530

    Update script4.sh

--- u_install/script4.sh
+++ u_install/script4.sh
@@ -58,7 +58,7 @@ cd /home/ashok/localai
 # PReparing docker for GPU
 # Ref StackOverflow: https://stackoverflow.com/a/77269071
 
-# 1.0 Configure the repository:
+# 1.0 Configure the repository (it is one command):
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey |sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
 && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list \
 && sudo apt-get update
