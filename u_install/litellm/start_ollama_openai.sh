5c643ba97066d1c8f16167c22d91efe62295503a
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-04-11 12:40:23 +0530

    Update start_ollama_openai.sh

--- u_install/litellm/start_ollama_openai.sh
+++ u_install/litellm/start_ollama_openai.sh
@@ -2,4 +2,13 @@
 # Shift to python virtual environment
 source /home/ashok/langchain/bin/activate
 # Let litellm server access ollama through its API
+echo " "
+echo "========"
+echo "Litellm would make ollama model behave as if it"
+echo "is an OpenAI model. Ollama model being accessed is:"
+echo "mistral:latest."
+echo "Change this model by editing file: ~/litellm/config_ollama.yaml"
+echo "========="
+echo " "
+sleep 5
 litellm --config /home/ashok/litellm/config_ollama.yaml
