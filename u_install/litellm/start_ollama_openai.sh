fc0c6adbcdd99625703dbb0768aae295d93c6ff6
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-04-11 12:57:49 +0530

    Update start_ollama_openai.sh

--- u_install/litellm/start_ollama_openai.sh
+++ u_install/litellm/start_ollama_openai.sh
@@ -7,8 +7,11 @@ echo "========"
 echo "Litellm would make ollama model behave as if it"
 echo "is an OpenAI model. Ollama model being accessed is:"
 echo "mistral:latest."
-echo "Change this model by editing file: ~/litellm/config_ollama.yaml"
+echo "Change this model by editing file: ~/litellm/config_ollama.yaml."
+echo "In flowise, in node, 'ChatLocaAI', write URL as: http://localhost:4000"
+echo "And 'Model Name' as: mistral:latest"
 echo "========="
 echo " "
 sleep 5
-litellm --config /home/ashok/litellm/config_ollama.yaml
+litellm --config /home/ashok/litellm/config_ollama.yaml > /dev/null 2>&1 &
+netstat -aunt | grep 4000
