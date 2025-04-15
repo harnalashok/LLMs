cbdcfc415b64911b8a4b9e130a7473444ade35ea
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-04-15 11:54:13 +0530

    Update script7.sh

--- u_install/script7.sh
+++ u_install/script7.sh
@@ -146,135 +146,136 @@ mv /home/ashok/next/script8.sh   /home/ashok/
 # A new venv for python3.11 is created
 #################
 
-echo "Install OpenWebUI? [Y,n]"
+echo "Shall I install OpenWebUI? [Y,n]"
 read input
 if [[ $input == "Y" || $input == "y" ]]; then
-        echo "do that"
-
-
-
-# Install openwebui using pip. Refer its github site
-# Ref: https://github.com/open-webui/open-webui?tab=readme-ov-file#installation-via-python-pip-
-# OpenWebUI utilizes Ollama to run GGUF models, so you'll need to be familiar 
-#  with Ollama's functionality to fully manage your GGUF models
-
-echo " "
-echo "---------------"
-echo "Will begin installing open-webui. Python version needed is exactly 3.11"
-echo "Will create a venv for python3.11"
-echo "Note: Open WebUI connects to Ollama to work with"
-echo "Better first install ollama if not already installed."
-echo "---------------"
-echo " "
-sleep 9
-# Check existing installed python versions on your machine
-echo "Existing python versions on your machine are are as below"
-echo " "
-
-ls /usr/bin/ | grep python
-
-read -p "Do you want to continue to install python3.11 ? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    exit 1
-fi
-echo " "
-echo "---------"
-echo "Installing python3.11"
-echo "Open Webui needs python3.11"
-echo "----- "
-sleep 4
-
-# Install python 3.11 now
-sudo apt update
-sudo apt install python3.11 -y
-
-# Install tool to create python venv
-# invokable with its own Python executable
-
-sudo apt install python3.11-venv  -y
-
-# Check again python versions
-echo "Installed python versions now are:"
-echo " "
-ls /usr/bin/ | grep python
-sleep 9
-
-# Create python virtual env at openwebui
-echo "Creating python virtual env at openwebui"
-# Using python3.11 package
-python3.11 -m venv /home/ashok/openwebui
-
-echo "Activating the new python env"
-# Activate the python env
-source /home/ashok/openwebui/bin/activate
-sleep 2
-
-echo "--------------"
-echo "Installing openwebui....Takes lots of time...."
-echo "If it breaks, try again....."
-echo "--------------"
-echo " "
-sleep 9
-
-# Installing openwebui. Takes time
-pip3.11 install open-webui
-
-echo " "
-echo " "
-echo "------------"
-echo "Done. Starting openwebui........"
-echo "During initial startup it imports many files"
-echo "Access it at port 7860"
-echo "You can kill it after starting with ctrl+c"
-echo "------------"
-echo " "
-sleep 9
-
-# Create script to start openwebui thenext time
-# Activate the env
-echo "source /home/ashok/openwebui/bin/activate" >  /home/ashok/start/start_openwebui.sh
-echo "open-webui serve"                          >> /home/ashok/start/start_openwebui.sh
-
-chmod +x /home/ashok/*.sh
-chmod +x /home/ashok/start/*.sh
-chmod +x /home/ashok/stop/*.sh
-
-
-# Start open-webui once
-open-webui serve
-
-deactivate
+      # Install openwebui using pip. Refer its github site
+      # Ref: https://github.com/open-webui/open-webui?tab=readme-ov-file#installation-via-python-pip-
+      # OpenWebUI utilizes Ollama to run GGUF models, so you'll need to be familiar 
+      #  with Ollama's functionality to fully manage your GGUF models
+      
+      echo " "
+      echo "---------------"
+      echo "Will begin installing open-webui. Python version needed is exactly 3.11"
+      echo "Will create a venv for python3.11"
+      echo "Note: Open WebUI connects to Ollama to work with"
+      echo "Better first install ollama if not already installed."
+      echo "---------------"
+      echo " "
+      sleep 9
+      # Check existing installed python versions on your machine
+      echo "Existing python versions on your machine are are as below"
+      echo " "
+      
+      ls /usr/bin/ | grep python
+      
+      read -p "Do you want to continue to install python3.11 ? " -n 1 -r
+      echo    # (optional) move to a new line
+      if [[ ! $REPLY =~ ^[Yy]$ ]]
+      then
+          exit 1
+      fi
+      echo " "
+      echo "---------"
+      echo "Installing python3.11"
+      echo "Open Webui needs python3.11"
+      echo "----- "
+      sleep 4
+      
+      # Install python 3.11 now
+      sudo apt update
+      sudo apt install python3.11 -y
+      
+      # Install tool to create python venv
+      # invokable with its own Python executable
+      
+      sudo apt install python3.11-venv  -y
+      
+      # Check again python versions
+      echo "Installed python versions now are:"
+      echo " "
+      ls /usr/bin/ | grep python
+      sleep 9
+      
+      # Create python virtual env at openwebui
+      echo "Creating python virtual env at openwebui"
+      # Using python3.11 package
+      python3.11 -m venv /home/ashok/openwebui
+      
+      echo "Activating the new python env"
+      # Activate the python env
+      source /home/ashok/openwebui/bin/activate
+      sleep 2
+      
+      echo "--------------"
+      echo "Installing openwebui....Takes lots of time...."
+      echo "If it breaks, try again....."
+      echo "--------------"
+      echo " "
+      sleep 9
+      
+      # Installing openwebui. Takes time
+      pip3.11 install open-webui
+      
+      echo " "
+      echo " "
+      echo "------------"
+      echo "Done. Starting openwebui........"
+      echo "During initial startup it imports many files"
+      echo "Access it at port 7860"
+      echo "You can kill it after starting with ctrl+c"
+      echo "------------"
+      echo " "
+      sleep 9
+      
+      # Create script to start openwebui thenext time
+      # Activate the env
+      echo "source /home/ashok/openwebui/bin/activate" >  /home/ashok/start/start_openwebui.sh
+      echo "open-webui serve"                          >> /home/ashok/start/start_openwebui.sh
+      
+      chmod +x /home/ashok/*.sh
+      chmod +x /home/ashok/start/*.sh
+      chmod +x /home/ashok/stop/*.sh
+    
+      echo "Will start open-webui server"
+      # Start open-webui once
+      open-webui serve
+      
+      deactivate
 
 else
-        echo "don't do that"
+        echo "Skipping install of OpenWebUI"
 fi
-
 
 
 #################
 # Create python3.13 virtual env
 #################
 
-# What all python versions are installed:
-ls /usr/bin | grep python
-
-sudo apt install python3.13 -y
-sudo apt-get install python3.13-venv -y
-python3.13 -m venv /home/ashok/python313
-# Create script to activate 'python313' env
-echo "echo 'To activate python3.13 virtual envs, activate as:' "            > /home/ashok/activate_python313_venv.sh
-echo "echo 'source /home/ashok/python313/bin/activate' "                   >>  /home/ashok/activate_python313_venv.sh
-echo "echo '(Note the change in prompt after activating)' "                >> /home/ashok/activate_python313_venv.sh
-echo "echo '(To deactivate, just enter the command: deactivate)' "         >> /home/ashok/activate_python313_venv.sh
-echo "source /home/ashok/python313/bin/activate"                           >> /home/ashok/activate_python313_venv.sh
-chmod +x /home/ashok/*.sh
-sleep 2
-
-cp /home/ashok/activate_python313_venv.sh  /home/ashok/start/activate_python313_venv.sh
-cp /home/ashok/activate_python313_venv.sh  /home/ashok/stop/activate_python313_venv.sh
-
+echo "Shall I create a virtual env for python3.13? [Y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+      echo "Here are a list of python versions installed on your machine"
+      # What all python versions are installed:
+      ls /usr/bin | grep python
+      sleep 5
+      sudo apt install python3.13 -y
+      sudo apt-get install python3.13-venv -y
+      python3.13 -m venv /home/ashok/python313
+      # Create script to activate 'python313' env
+      echo "echo 'To activate python3.13 virtual envs, activate as:' "            > /home/ashok/activate_python313_venv.sh
+      echo "echo 'source /home/ashok/python313/bin/activate' "                   >>  /home/ashok/activate_python313_venv.sh
+      echo "echo '(Note the change in prompt after activating)' "                >> /home/ashok/activate_python313_venv.sh
+      echo "echo '(To deactivate, just enter the command: deactivate)' "         >> /home/ashok/activate_python313_venv.sh
+      echo "source /home/ashok/python313/bin/activate"                           >> /home/ashok/activate_python313_venv.sh
+      chmod +x /home/ashok/*.sh
+      sleep 2
+      
+      cp /home/ashok/activate_python313_venv.sh  /home/ashok/start/activate_python313_venv.sh
+      cp /home/ashok/activate_python313_venv.sh  /home/ashok/stop/activate_python313_venv.sh
+else
+        echo "Skipping install of OpenWebUI"
+fi
 #=========
 
 echo "You may like to execute:"
