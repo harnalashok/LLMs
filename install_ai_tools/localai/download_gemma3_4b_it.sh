83b2c493a38180165686f44c308e45d2ccce0d5c
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-07-10 14:48:28 +0530

    Update and rename download_localaiModel2.sh to download_gemma3_4b_it.sh

--- install_ai_tools/localai/download_gemma3_4b_it.sh
+++ install_ai_tools/localai/download_gemma3_4b_it.sh
@@ -4,15 +4,16 @@
 # Better download it directly from localai models gallery
 # It will be named as gpt-3.5-turbo
 
-
+echo "Will download: gemma-3-4b-it"
 LOCALAI=http://localhost:8080
 curl $LOCALAI/models/apply -H "Content-Type: application/json" -d '{
-     "id": "LocalAI-functioncall-phi-4-v0.3",
+     "id": "gemma-3-4b-it",
       "name": "gpt-3.5-turbo"
    }'
 
 echo " "
-echo " "
+echo "Model is: gemma-3-4b-it"
+echo "Model has a new name: gpt-3.5-turbo "
 echo "Downloaded models are stored here:"
 echo "       /var/lib/docker/volumes/12518350e14a619d6c5412073fec2a2988d41ea582780244d168c6d8ccef5229/_data/"
 echo "To check download status, execute: ./get_download_status.sh "   
