fc9b53e40fd12fff789a79660b0e7880cc70caaa
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-07-19 12:21:17 +0530

    Update ollama_flowise_chroma_n8n.sh

--- install_ai_tools/quick_flowise_ollama/ollama_flowise_chroma_n8n.sh
+++ install_ai_tools/quick_flowise_ollama/ollama_flowise_chroma_n8n.sh
@@ -166,6 +166,7 @@ if [[ $input == "Y" || $input == "y" ]]; then
     echo " "                                                                                                                  >> /home/$USER/start/start_docker_n8n.sh
     echo "echo 'Access n8n at port 5678. Wait...starting...'"                                                                 >> /home/$USER/start/start_docker_n8n.sh
     echo "echo 'To stop it, issue command: cd /home/$USER/n8n/ ; docker stop n8n'"                                             >> /home/$USER/start/start_docker_n8n.sh
+    echo "echo 'Use \"top -u $USER\" command to see memory usage'"                                                             >>  /home/ashok/start/start_docker_n8n.sh
     echo "sleep 9"                                                                                                             >> /home/$USER/start/start_docker_n8n.sh
     echo "cd /home/$USER/n8n"                                                                                                  >> /home/$USER/start/start_docker_n8n.sh
     echo "docker run -d -it --rm --name n8n -p 5678:5678  -e NODE_OPTIONS=\"--max-old-space-size=4096\"  -v /home/$USER/n8n_data:/home/$USER/n8n/node/.n8n docker.n8n.io/n8nio/n8n"   >> /home/$USER/start/start_docker_n8n.sh
