a335eb083ac5d8f4ae44f9cc578fd104e5ebd28d
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-07-20 09:07:12 +0530

    Update script2.sh

--- install_ai_tools/script2.sh
+++ install_ai_tools/script2.sh
@@ -13,15 +13,15 @@ sleep 10
 
 cd ~/
 
-echo " "                                      | tee -a /home/ashok/error.log
-echo "*********"                              | tee -a /home/ashok/error.log
-echo "Script: script2.sh"                     | tee -a /home/ashok/error.log
-echo "**********"                             | tee -a /home/ashok/error.log
-echo " "                                      | tee -a /home/ashok/error.log
+echo " "                                      | tee -a /home/$USER/error.log
+echo "*********"                              | tee -a /home/$USER/error.log
+echo "Script: script2.sh"                     | tee -a /home/$USER/error.log
+echo "**********"                             | tee -a /home/$USER/error.log
+echo " "                                      | tee -a /home/$USER/error.log
 
 # Check if llama.cpp is already installed?
 
-DIRECTORY="/home/ashok/llama.cpp"
+DIRECTORY="/home/$USER/llama.cpp"
 if [ -d "$DIRECTORY" ]; then
   echo "$DIRECTORY does exist."
   echo "Recheck if script2.sh was executed earlier"
@@ -37,10 +37,10 @@ echo "Shall I install llama.cpp (Can be safely skipped)? [Y,n]"    # Else docker
 read input
 if [[ $input == "Y" || $input == "y" ]]; then
   # Installing llama.cpp
-  echo " "                                         | tee -a /home/ashok/error.log
-  echo "Installing llama.cpp"                      | tee -a /home/ashok/error.log
-  echo "------------"                              | tee -a /home/ashok/error.log
-  echo " "                                         | tee -a /home/ashok/error.log
+  echo " "                                         | tee -a /home/$USER/error.log
+  echo "Installing llama.cpp"                      | tee -a /home/$USER/error.log
+  echo "------------"                              | tee -a /home/$USER/error.log
+  echo " "                                         | tee -a /home/$USER/error.log
   git clone https://github.com/ggerganov/llama.cpp
   cd llama.cpp
   cmake -B build
@@ -48,33 +48,33 @@ if [[ $input == "Y" || $input == "y" ]]; then
   cd ~
   sleep 2
   # Create a symlink to models and to gguf folder
-  ln -s /home/ashok/llama.cpp/models/ /home/ashok/
-  ln -s /home/ashok/llama.cpp/models/ /home/ashok/gguf
+  ln -s /home/$USER/llama.cpp/models/ /home/$USER/
+  ln -s /home/$USER/llama.cpp/models/ /home/$USER/gguf
   
-  echo "PATH=\$PATH:/home/ashok/llama.cpp/build/bin" >> .bashrc
-  echo " "                                        | tee -a /home/ashok/error.log
-  echo "-------"                                  | tee -a /home/ashok/error.log
-  echo "llama.cpp installed"                      | tee -a /home/ashok/error.log
-  echo "10. llama.cpp installed"                  | tee -a /home/ashok/info.log
-  echo "-------"                                  | tee -a /home/ashok/error.log
+  echo "PATH=\$PATH:/home/$USER/llama.cpp/build/bin" >> .bashrc
+  echo " "                                        | tee -a /home/$USER/error.log
+  echo "-------"                                  | tee -a /home/$USER/error.log
+  echo "llama.cpp installed"                      | tee -a /home/$USER/error.log
+  echo "10. llama.cpp installed"                  | tee -a /home/$USER/info.log
+  echo "-------"                                  | tee -a /home/$USER/error.log
   
   
   # Script to start llama.cpp server
-  echo '#!/bin/bash'                                         | tee    /home/ashok/start/start_llamacpp_server.sh
-  echo " "                                                   | tee -a /home/ashok/start/start_llamacpp_server.sh
-  echo "cd ~/"                                               | tee -a /home/ashok/start/start_llamacpp_server.sh
-  echo " "                                                   | tee -a /home/ashok/start/start_llamacpp_server.sh
-  echo "echo 'llama.cpp server will be available at port: 8080'"            | tee -a /home/ashok/start/start_llamacpp_server.sh
-  echo "echo 'Script will use model: llama-thinker-3b-preview-q8_0.gguf'"   | tee -a /home/ashok/start/start_llamacpp_server.sh
-  echo "echo 'Change it, if you like, by changing the script'"              | tee -a /home/ashok/start/start_llamacpp_server.sh
-  echo " "                                                                  | tee -a /home/ashok/start/start_llamacpp_server.sh
-  echo "sleep 10"                                                           | tee -a /home/ashok/start/start_llamacpp_server.sh
-  echo "llama-server -m ~/gguf/llama-thinker-3b-preview-q8_0.gguf -c 2048"  | tee -a /home/ashok/start/start_llamacpp_server.sh
+  echo '#!/bin/bash'                                         | tee    /home/$USER/start/start_llamacpp_server.sh
+  echo " "                                                   | tee -a /home/$USER/start/start_llamacpp_server.sh
+  echo "cd ~/"                                               | tee -a /home/$USER/start/start_llamacpp_server.sh
+  echo " "                                                   | tee -a /home/$USER/start/start_llamacpp_server.sh
+  echo "echo 'llama.cpp server will be available at port: 8080'"            | tee -a /home/$USER/start/start_llamacpp_server.sh
+  echo "echo 'Script will use model: llama-thinker-3b-preview-q8_0.gguf'"   | tee -a /home/$USER/start/start_llamacpp_server.sh
+  echo "echo 'Change it, if you like, by changing the script'"              | tee -a /home/$USER/start/start_llamacpp_server.sh
+  echo " "                                                                  | tee -a /home/$USER/start/start_llamacpp_server.sh
+  echo "sleep 10"                                                           | tee -a /home/$USER/start/start_llamacpp_server.sh
+  echo "llama-server -m ~/gguf/llama-thinker-3b-preview-q8_0.gguf -c 2048"  | tee -a /home/$USER/start/start_llamacpp_server.sh
 else
   echo "Skipping install of llama.cpp"
 fi
   
-chmod +x /home/ashok/start/*.sh
+chmod +x /home/$USER/start/*.sh
 
 ##########
 # Using gguf model by Ollama
@@ -85,9 +85,9 @@ chmod +x /home/ashok/start/*.sh
 # You can then issue the command to use a gguf model within ollama:
 # See file help.txt for more details.
 
-echo "FROM ~/gguf/llama-2-13b-chat.Q4_K_M.gguf"  > /home/ashok/Modelfile
+echo "FROM ~/gguf/llama-2-13b-chat.Q4_K_M.gguf"  > /home/$USER/Modelfile
 
-#  ollama create <YourModelName> -f /home/ashok/Modelfile
+#  ollama create <YourModelName> -f /home/$USER/Modelfile
 # Note ollama blobs are stored at:  /usr/share/ollama/.ollama/models/blobs/
 
 #################
@@ -96,30 +96,30 @@ echo "FROM ~/gguf/llama-2-13b-chat.Q4_K_M.gguf"  > /home/ashok/Modelfile
 
 # 1.2 download and install Node.js
 
-echo " "                                       | tee -a /home/ashok/error.log
-echo "-------"                                 | tee -a /home/ashok/error.log
-echo "Installing Node.js ver 20......"         | tee -a /home/ashok/error.log
-echo "-------"                                 | tee -a /home/ashok/error.log
-fnm use --install-if-missing 20                2>> /home/ashok/error.log
-echo " "                                       | tee -a /home/ashok/error.log
-echo "Node.js installed"                       | tee -a /home/ashok/error.log
-echo "11. Node.js installed"                   | tee -a /home/ashok/error.log
+echo " "                                       | tee -a /home/$USER/error.log
+echo "-------"                                 | tee -a /home/$USER/error.log
+echo "Installing Node.js ver 20......"         | tee -a /home/$USER/error.log
+echo "-------"                                 | tee -a /home/$USER/error.log
+fnm use --install-if-missing 20                2>> /home/$USER/error.log
+echo " "                                       | tee -a /home/$USER/error.log
+echo "Node.js installed"                       | tee -a /home/$USER/error.log
+echo "11. Node.js installed"                   | tee -a /home/$USER/error.log
 node -v
-echo "------------"                            | tee -a /home/ashok/error.log
-echo "  "                                      | tee -a /home/ashok/error.log
+echo "------------"                            | tee -a /home/$USER/error.log
+echo "  "                                      | tee -a /home/$USER/error.log
 
 # Refer https://github.com/Schniz/fnm#shell-setup
-echo " "                                            >>  /home/ashok/.bashrc
-echo 'eval "$(fnm env --use-on-cd --shell bash)"'   >>  /home/ashok/.bashrc
-echo " "                                            >>  /home/ashok/.bashrc
+echo " "                                            >>  /home/$USER/.bashrc
+echo 'eval "$(fnm env --use-on-cd --shell bash)"'   >>  /home/$USER/.bashrc
+echo " "                                            >>  /home/$USER/.bashrc
 sleep 6
 
 # Move script file to done folder
-mv /home/ashok/script2.sh /home/ashok/done
-mv /home/ashok/next/ubuntu_docker1.sh /home/ashok/
+mv /home/$USER/script2.sh /home/$USER/done
+mv /home/$USER/next/ubuntu_docker1.sh /home/$USER/
 
 # Add start/stop folders to PATH in .bashrc
-echo "PATH=\$PATH:/home/ashok/start:/home/ashok/stop"  >> /home/ashok/.bashrc
+echo "PATH=\$PATH:/home/$USER/start:/home/$USER/stop"  >> /home/$USER/.bashrc
 
 
 ###############
