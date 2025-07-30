51ceef54755eda6206f6e6a4d740c885ebed916c
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-07-30 11:58:44 +0530

    Update ollama_flowise_chroma_n8n.sh

--- install_ai_tools/quick_flowise_ollama/ollama_flowise_chroma_n8n.sh
+++ install_ai_tools/quick_flowise_ollama/ollama_flowise_chroma_n8n.sh
@@ -479,10 +479,18 @@ if [[ $input == "Y" || $input == "y" ]]; then
    #On error
    #sudo systemctl daemon-reload
    # Mongosh
+   echo "Will install mongosh"
+   sleep 4
    wget -qO- https://www.mongodb.org/static/pgp/server-8.0.asc | sudo tee /etc/apt/trusted.gpg.d/server-8.0.asc
    sudo apt-get install gnupg
    wget -qO- https://www.mongodb.org/static/pgp/server-8.0.asc | sudo tee /etc/apt/trusted.gpg.d/server-8.0.asc
    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+   sudo apt-get update
+   sudo apt-get install -y mongodb-mongosh
+   mongosh --version
+else
+    echo "Mongodb not installed"
+fi    
 
 
 
