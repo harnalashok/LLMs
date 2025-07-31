e11d5c2ecdcd6151b07f0a2622203904062f3153
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-07-31 10:41:41 +0530

    Update ollama_flowise_chroma_n8n.sh

--- install_ai_tools/quick_flowise_ollama/ollama_flowise_chroma_n8n.sh
+++ install_ai_tools/quick_flowise_ollama/ollama_flowise_chroma_n8n.sh
@@ -165,7 +165,7 @@ if [[ $input == "Y" || $input == "y" ]]; then
     docker volume create n8n_data
     #   https://nodejs.org/api/cli.html#--max-old-space-sizesize-in-megabytes
     #   https://docs.n8n.io/hosting/scaling/memory-errors/#increase-old-memory
-    # Access at localhost:45678
+    # Access at localhost:5678
     # --rm implies remove docker when stopped. So docker will not show up in 'docker ps -a' call
     docker run -it -d --rm --name n8n -p 45678:5678 -e NODE_OPTIONS="--max-old-space-size=4096" --network host  -v n8n_data:/home/$USER/n8n/node/.n8n docker.n8n.io/n8nio/n8n
     # Access at localhost:5678
@@ -173,12 +173,12 @@ if [[ $input == "Y" || $input == "y" ]]; then
     # n8n start script for Ubuntu
     echo '#!/bin/bash'                                                                                                        > /home/$USER/start_n8n.sh
     echo " "                                                                                                                  >> /home/$USER/start_n8n.sh
-    echo "echo 'Access n8n at port 45678. Wait...starting...'"                                                                 >> /home/$USER/start_n8n.sh
+    echo "echo 'Access n8n at port 5678. Wait...starting...'"                                                                 >> /home/$USER/start_n8n.sh
     echo "echo 'To stop it, issue command: cd /home/$USER/n8n/ ; docker stop n8n'"                                             >> /home/$USER/start_n8n.sh
-    echo "echo 'Use \"top -u $USER\" OR \"free -g \" command to see memory usage'"                                                             >>  /home/$USER/start_n8n.sh
+    echo "echo 'Use \"top -u $USER\" OR \"free -g \" command to see memory usage'"                                             >>  /home/$USER/start_n8n.sh
     echo "sleep 9"                                                                                                             >> /home/$USER/start_n8n.sh
     echo "cd /home/$USER/n8n"                                                                                                  >> /home/$USER/start_n8n.sh
-    echo "docker run -d -it --rm --name n8n -p 45678:5678  -e NODE_OPTIONS=\"--max-old-space-size=4096\"  -v /home/$USER/n8n_data:/home/$USER/n8n/node/.n8n docker.n8n.io/n8nio/n8n"   >> /home/$USER/start_n8n.sh
+    echo "docker run -d -it --rm --name n8n -p 5678:5678  -e NODE_OPTIONS=\"--max-old-space-size=4096\"  -v /home/$USER/n8n_data:/home/$USER/n8n/node/.n8n docker.n8n.io/n8nio/n8n"   >> /home/$USER/start_n8n.sh
     
     # n8n start script for WSL
     echo '#!/bin/bash'                                                                                                         > /home/$USER/start_wsl_n8n.sh
