9c3d03145b554f8e17000e61533e509fe7007016
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-08-08 18:55:43 +0530

    Update script9.sh

--- install_ai_tools/script9.sh
+++ install_ai_tools/script9.sh
@@ -250,51 +250,47 @@ echo "netstat -aunt | grep 7860"                           >> /home/$USER/stop/s
 # portrainer docker
 ######################
 
-# Installing portrainer
-echo "Installinh portrainer docker in directory portrainer"  | tee -a /home/$USER/info.log
-cd ~/
-mkdir /home/$USER/portainer
-cd /home/$USER/portainer/
-docker volume create portainer_data
-# This is one long line command
-# To change port 8000 to a different value, see: https://github.com/portainer/portainer-docs/issues/91#issuecomment-1184225862
+cd /home/$USER
+echo " "
+echo " "
+echo "------------"        
+echo "Shall I install portainer docker? [Y,n]"    # Else docker chromadb may be installed
+read input
+input=${input:-Y}
+if [[ $input == "Y" || $input == "y" ]]; then
+   # Installing portrainer
+   echo "Installing portainer docker "                             | tee -a /home/$USER/info.log
+   # Script to start portainer container
+   echo '#!/bin/bash'                                              > /home/$USER/start/start_portainer.sh
+   echo " "                                                       >> /home/$USER/start/start_portainer.sh
+   echo "cd /home/$USER"                                          >> /home/$USER/start/start_portainer.sh
+   echo "echo '#========'"                                        >> /home/$USER/start/start_portainer.sh
+   echo "echo '#Access portainer at:'"                            >> /home/$USER/start/start_portainer.sh
+   echo "echo '#https://127.0.0.1:9443'"                          >> /home/$USER/start/start_portainer.sh
+   echo "echo '#User: admin; password: foreschoolmgt'"            >> /home/$USER/start/start_portainer.sh
+   echo "echo '#=========='"                                      >> /home/$USER/start/start_portainer.sh
+   #echo "cd /home/$USER/portainer/"                               >> /home/$USER/start/start_portainer.sh
+   echo "docker start portainer"                                  >> /home/$USER/start/start_portainer.sh
+   echo "netstat -aunt | grep 9443"                               >> /home/$USER/start/start_portainer.sh
+   
+   echo '#!/bin/bash'                                              > /home/$USER/stop/stop_portainer.sh
+   echo " "                                                       >> /home/$USER/stop/stop_portainer.sh
+   echo "cd /home/$USER"                                          >> /home/$USER/stop/stop_portainer.sh
+   #echo "cd /home/$USER/portainer/"                               >> /home/$USER/stop/stop_portainer.sh
+   echo "docker stop portainer"                                   >> /home/$USER/stop/stop_portainer.sh
+   echo "netstat -aunt | grep 9443"                               >> /home/$USER/stop/stop_portainer.sh
 
-<<comment
-cd /home/$USER/portainer
-docker rm portainer
-docker volume create portainer_data
-#docker run -d -p 8888:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.5
-docker run -d -p 8888:8000 -p 9443:9443 --name portainer -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.5
-comment
-
-# Install portainer community edition
-#docker run -d -p 8888:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.5
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
-cd ~/
-# Script to start portainer container
-
-
-echo '#!/bin/bash'                                              > /home/$USER/start/start_portainer.sh
-echo " "                                                       >> /home/$USER/start/start_portainer.sh
-echo "cd ~/"                                                   >> /home/$USER/start/start_portainer.sh
-echo "echo '#========'"                                        >> /home/$USER/start/start_portainer.sh
-echo "echo '#Access portainer at:'"                            >> /home/$USER/start/start_portainer.sh
-echo "echo '#https://127.0.0.1:9443'"                          >> /home/$USER/start/start_portainer.sh
-echo "echo '#User: admin; password: foreschoolmgt'"            >> /home/$USER/start/start_portainer.sh
-echo "echo '#=========='"                                      >> /home/$USER/start/start_portainer.sh
-echo "cd /home/$USER/portainer/"                               >> /home/$USER/start/start_portainer.sh
-echo "docker start portainer"                                  >> /home/$USER/start/start_portainer.sh
-echo "netstat -aunt | grep 9443"                               >> /home/$USER/start/start_portainer.sh
-
-echo '#!/bin/bash'                                              > /home/$USER/stop/stop_portainer.sh
-echo " "                                                       >> /home/$USER/stop/stop_portainer.sh
-echo "cd ~/"                                                   >> /home/$USER/stop/stop_portainer.sh
-echo "cd /home/$USER/portainer/"                               >> /home/$USER/stop/stop_portainer.sh
-echo "docker stop portainer"                                   >> /home/$USER/stop/stop_portainer.sh
-echo "netstat -aunt | grep 9443"                               >> /home/$USER/stop/stop_portainer.sh
-
-
-
+   cd /home/$USER
+   docker volume create portainer_data
+   # This is one long line command
+   # To change port 8000 to a different value, see: https://github.com/portainer/portainer-docs/issues/91#issuecomment-1184225862
+   # Install portainer community edition
+   #docker run -d -p 8888:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.5
+   docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
+else
+   echo "Portainer not installed"
+fi   
+   
 # Move script file to done folder
 cd ~/
 chmod +x *.sh
