456fd0d8f15aaf07ab1422791d88bc2a1a459917
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-08-14 05:32:44 +0530

    Update ollama_flowise_chroma_n8n.sh

--- install_ai_tools/quick_flowise_ollama/ollama_flowise_chroma_n8n.sh
+++ install_ai_tools/quick_flowise_ollama/ollama_flowise_chroma_n8n.sh
@@ -105,6 +105,40 @@ else
     sleep 15
  fi
 
+##################
+# Docker installation-I
+#################
+
+cd /home/$USER
+if [ ! -f /home/$USER/docker_installed.txt ]; then
+    # Ref: https://docs.docker.com/engine/install/ubuntu/
+    #      https://docs.docker.com/engine/install/linux-postinstall/
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+    # Add the repository to Apt sources:
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update -y
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin  -y
+    echo "Docker is installed" > /home/$USER/docker_installed.txt   # To avoid repeat installation
+    echo "Ubuntu will be closed/rebooted "
+    echo "After opening/restart, execute:"
+    sleep 9
+    if [[ -f /proc/sys/fs/binfmt_misc/WSLInterop ]]; then
+        wsl.exe --shutdown
+    else
+        reboot
+    fi  
+else
+   echo "Docker is installed"
+fi  
+
 
 
 
