fc595180b2b4e91ced1d00b4dfdb446f36f9c105
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-08-14 05:41:18 +0530

    Update ollama_flowise_chroma_n8n.sh

--- install_ai_tools/quick_flowise_ollama/ollama_flowise_chroma_n8n.sh
+++ install_ai_tools/quick_flowise_ollama/ollama_flowise_chroma_n8n.sh
@@ -140,14 +140,64 @@ else
    echo "Docker is installed"
 fi  
 
+##################
+# Docker installation-II
+#################
 
-
-
-
-
-
- 
-
+cd /home/$USER
+if [ ! -f /home/$USER/docker_installed_1.txt ]; then
+    # Ref: https://docs.docker.com/engine/install/ubuntu/
+    #      https://docs.docker.com/engine/install/linux-postinstall/
+    # Check if docker installed
+    sudo docker run hello-world
+    # Run docker witout root privileges
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
+    #
+    sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+    sudo chmod g+rwx "$HOME/.docker" -R
+    #
+    sudo systemctl disable docker.service
+    sudo systemctl disable containerd.service
+    #
+    # PReparing docker for GPU
+    # Ref StackOverflow: https://stackoverflow.com/a/77269071
+    # Ref: https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html#installation
+    # 1.0 Configure the repository (it is one command):
+    curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey |sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+    && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list \
+    && sudo apt-get update
+    #
+    # 2.0 Install the NVIDIA Container Toolkit packages:
+    #
+    sudo apt-get install -y nvidia-container-toolkit
+    #
+    # 3.0  Configure the container runtime by using the nvidia-ctk command:
+    #
+    sudo nvidia-ctk runtime configure --runtime=docker
+    #
+    # 4.0 Restart the Docker daemon:
+    #
+    sudo systemctl restart docker
+    #
+    # Store docker help files
+    mkdir /home/$USER/Documents/dockers
+    cd /home/$USER/Documents/dockers
+    wget -c https://github.com/harnalashok/LLMs/blob/main/install_ai_tools/docker/Understanding%20docker%20technology.pdf?raw=true
+    wget -c https://github.com/harnalashok/LLMs/blob/main/install_ai_tools/docker/docker%20commands.txt?raw=true
+    wget -c https://github.com/harnalashok/LLMs/blob/main/install_ai_tools/docker/dockers%20in%20brief.pdf?raw=true
+    cd /home/$USER/
+    #
+    echo "Docker installation completed" > /home/$USER/docker_installed_1.txt   # To avoid repeat installation
+    echo "Machine will be rebooted "
+    if [[ -f /proc/sys/fs/binfmt_misc/WSLInterop ]]; then
+        wsl.exe --shutdown
+    else
+        reboot
+    fi  
+else
+    echo "Docker installation process completed"
+fi    
 
 
 ##############
