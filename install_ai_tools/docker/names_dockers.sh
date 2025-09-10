2c0e18f31de5c7368ba8084b1ce9e6664ea270f6
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-09-10 13:22:29 +0530

    Update names_dockers.sh

--- install_ai_tools/docker/names_dockers.sh
+++ install_ai_tools/docker/names_dockers.sh
@@ -14,6 +14,8 @@ cd /home/$USER/ragflow/docker
 docker compose ps -a --format "{{.Names}}"
 echo " "
 echo "====="
+echo "RagFlow Containers aloing with imageIDs"
+docker compose images  | awk  '{print $1 "\t"  $5 "\t"  $2}'
 cd /home/$USER
 
 
