09cb9183adcf344b75e94fd189f05d656af751fc
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-11-11 11:16:02 +0530

    Update ollama_flowise_chroma_n8n_ubuntu.sh

--- install_ai_tools/quick_flowise_ollama/ollama_flowise_chroma_n8n_ubuntu.sh
+++ install_ai_tools/quick_flowise_ollama/ollama_flowise_chroma_n8n_ubuntu.sh
@@ -93,6 +93,7 @@ if [ ! -f /home/$USER/cuda_installed.txt ]; then
     echo " "
     echo "  "
     echo "==>For Ubuntu 22.04 ONLY<=="
+	echo "https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=22.04&target_type=deb_local"
     echo "Shall I install NVIDIA Toolkit (cuda-13) for Ubuntu-22.04? [Y,n]"    
     read input
     input=${input:-Y}
@@ -100,12 +101,12 @@ if [ ! -f /home/$USER/cuda_installed.txt ]; then
        # Remove old gpg key
         sudo apt-key del 7fa2af80
         # Now follow the instructions as on this page:
-        #  https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=WSL-Ubuntu&target_version=2.0&target_type=deb_local
+        #  https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=22.04&target_type=deb_local
         # Added on 27th Sep, 2025
-       wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+       wwget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
        sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
-       wget https://developer.download.nvidia.com/compute/cuda/13.0.1/local_installers/cuda-repo-ubuntu2204-13-0-local_13.0.1-580.82.07-1_amd64.deb
-       sudo dpkg -i cuda-repo-ubuntu2204-13-0-local_13.0.1-580.82.07-1_amd64.deb
+       wget https://developer.download.nvidia.com/compute/cuda/13.0.2/local_installers/cuda-repo-ubuntu2204-13-0-local_13.0.2-580.95.05-1_amd64.deb
+       sudo dpkg -i cuda-repo-ubuntu2204-13-0-local_13.0.2-580.95.05-1_amd64.deb
        sudo cp /var/cuda-repo-ubuntu2204-13-0-local/cuda-*-keyring.gpg /usr/share/keyrings/
        sudo apt-get update
        sudo apt-get -y install cuda-toolkit-13-0
