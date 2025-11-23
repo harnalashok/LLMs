20aeda35a572fe1744b40406686ba1018c9a054f
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-11-23 13:09:51 +0530

    Update ollama_flowise_chroma_n8n_ubuntu.sh

--- install_ai_tools/quick_flowise_ollama/ollama_flowise_chroma_n8n_ubuntu.sh
+++ install_ai_tools/quick_flowise_ollama/ollama_flowise_chroma_n8n_ubuntu.sh
@@ -1505,7 +1505,10 @@ if [ ! -f /home/$USER/ragflow_installed.txt ]; then
 	    echo "cd /home/$USER"                                      >> /home/$USER/docker/del_rf_containers.sh
 	    echo "./volumes_ragflow.sh"                                >> /home/$USER/docker/del_rf_containers.sh   
 		echo "rm /home/$USER/ragflow_installed.txt"                >> /home/$USER/docker/del_rf_containers.sh  
-		echo "sudo rm -rf /home/$USER/ragflow"                          >> /home/$USER/docker/del_rf_containers.sh  
+		echo "sudo rm -rf /home/$USER/ragflow"                     >> /home/$USER/docker/del_rf_containers.sh 
+		echo "rm /home/$USER/docker/del_rf_containers.sh"          >> /home/$USER/docker/del_rf_containers.sh
+		echo "rm /home/$USER/rm_ragflow.sh"                        >> /home/$USER/docker/del_rf_containers.sh
+		echo "rm /home/$USER/delete_ragflow.sh"                    >> /home/$USER/docker/del_rf_containers.sh
 	    cd /home/$USER/docker
 	    chmod +x *.sh
 	    cd /home/$USER
