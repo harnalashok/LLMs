45708aa7b17df025af920a9061b523c72137af63
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-12-01 13:29:04 +0530

    Update ollama_flowise_chroma_n8n_ubuntu.sh

--- install_ai_tools/quick_flowise_ollama/ollama_flowise_chroma_n8n_ubuntu.sh
+++ install_ai_tools/quick_flowise_ollama/ollama_flowise_chroma_n8n_ubuntu.sh
@@ -358,7 +358,7 @@ if [ ! -f /home/$USER/anaconda_installed.txt ]; then
 cd /home/$USER
 # Only if anaconda is installed:
 if [  -f /home/$USER/anaconda_installed.txt ]; then
-	if [  -f /home/$USER/antigravity_installed.txt ]; then
+	if [ ! -f /home/$USER/antigravity_installed.txt ]; then
 		echo "Shall I latest Google Antigravity? [Y,n]"    
 		read input
 		input=${input:-Y}
