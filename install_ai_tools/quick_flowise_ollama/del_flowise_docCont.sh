997c943358af7f5a8c8d4682a4a16668826b89b1
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-12-28 18:49:31 +0530

    Update del_flowise_docCont.sh

--- install_ai_tools/quick_flowise_ollama/del_flowise_docCont.sh
+++ install_ai_tools/quick_flowise_ollama/del_flowise_docCont.sh
@@ -18,3 +18,32 @@ sleep 2
 docker start docker-flowise-1
 sleep 2
 netstat -aunt | grep 3000
+
+
+# Create files
+cd /home/$USER/
+echo '#!/bin/bash'                                         >  /home/$USER/start_flowise.sh
+echo " "                                                   >> /home/$USER/start_flowise.sh
+echo "cd ~/"                                               >> /home/$USER/start_flowise.sh
+echo "echo 'Flowise port 3000 onstarting'"                 >> /home/$USER/start_flowise.sh
+echo "echo 'Access flowise as: http://localhost:3000'"           >> /home/$USER/start_flowise.sh
+echo "cd /home/$USER/Flowise"                              >> /home/$USER/start_flowise.sh
+echo "docker start docker-flowise-1"                       >> /home/$USER/start_flowise.sh
+echo "sleep 3"                                             >> /home/$USER/start_flowise.sh
+echo "netstat -aunt | grep 3000"                           >> /home/$USER/start_flowise.sh
+# logs script
+echo '#!/bin/bash'                                         >  /home/$USER/logs_flowise.sh
+echo " "                                                   >> /home/$USER/logs_flowise.sh
+echo "cd /home/$USER/"                                     >> /home/$USER/logs_flowise.sh
+#echo "echo 'Flowise version is:'"                          >> /home/$USER/logs_flowise.sh
+#echo "docker logs flowise | grep start:default | head -1 | awk '{print \$2}'"  >> /home/$USER/logs_flowise.sh
+#echo "sleep 4"                                             >> /home/$USER/logs_flowise.sh
+echo "docker logs docker-flowise-1"                         >> /home/$USER/logs_flowise.sh
+# Stop script
+echo '#!/bin/bash'                                        >  /home/$USER/stop_docker_flowise.sh
+echo " "                                                  >> /home/$USER/stop_docker_flowise.sh
+echo "cd /home/$USER/"                                    >> /home/$USER/stop_docker_flowise.sh
+echo "echo 'Flowise Stopping'"                            >> /home/$USER/stop_docker_flowise.sh
+echo "cd /home/$USER/Flowise"                             >> /home/$USER/stop_docker_flowise.sh
+echo "docker stop docker-flowise-1"                                >> /home/$USER/stop_docker_flowise.sh
+echo "netstat -aunt | grep 3000"                           >> /home/$USER/stop_docker_flowise.sh
