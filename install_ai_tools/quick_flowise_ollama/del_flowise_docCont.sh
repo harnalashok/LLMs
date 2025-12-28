75b8784ff5dc0a7576975ef409fb37f42a967b72
harnalashok <47495816+harnalashok@users.noreply.github.com>
2025-12-28 18:32:11 +0530

    Create del_flowise_docCont.sh

---
+++ install_ai_tools/quick_flowise_ollama/del_flowise_docCont.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+# Delete container
+docker rm flowise
+# Delete image
+docker rmi flowise
+# create .env file
+cd /home/ashok/Flowise/docker
+cp .env.example  .env
+# Start docker
+docker compose up -d
+docker stop docker-flowise-1
+ 
