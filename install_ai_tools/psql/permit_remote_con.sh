238905ef7df01344912ffe1a002125f124b4aae1
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-01-15 10:50:11 +0530

    Update permit_remote_con.sh

--- install_ai_tools/psql/permit_remote_con.sh
+++ install_ai_tools/psql/permit_remote_con.sh
@@ -6,4 +6,12 @@ cd /etc/postgresql/$version/main
 echo "listen_addresses = '*'" |  sudo tee -a /etc/postgresql/$version/main/postgresql.conf
 echo "host    all             all             0.0.0.0/0               scram-sha-256" |  sudo tee -a /etc/postgresql/$version/main/pg_hba.conf
 sudo systemctl restart postgresql
+
+# Modified files are here:
+echo "pg_hba.conf file:"
+sleep 4
+sudo tail /etc/postgresql/$version/main/pg_hba.conf
+echo "postgresql.conf file:"
+sleep 4
+sudo tail /etc/postgresql/$version/main/postgresql.conf
         
