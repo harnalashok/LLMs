8f607b9e3355697a533715aee4d7c4d03385f31b
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-01-15 10:39:25 +0530

    Update permit_remote_con.sh

--- install_ai_tools/psql/permit_remote_con.sh
+++ install_ai_tools/psql/permit_remote_con.sh
@@ -2,7 +2,7 @@
 
 echo "Will change 'postgresql.conf' and 'pg_hba.conf' and make postgresql highly permissive"
 sleep 4
-version=$(psql -V | awk '{print $3}' |  cut -d '.' -f 1 | tr -d '\n')'
+version=$(psql -V | awk '{print $3}' |  cut -d '.' -f 1 | tr -d '\n')
 cd /etc/postgresql/$version/main
 echo "listen_addresses = '*'" |  sudo tee -a /etc/postgresql/$version/main/postgresql.conf
 echo "host    all             all             0.0.0.0/0               scram-sha-256" |  sudo tee -a /etc/postgresql/$version/main/pg_hba.conf
