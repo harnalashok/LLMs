6f78ca3068fcd6e688420405712263f2f26f23b4
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-01-24 14:07:09 +0530

    Update ollama_wsl_nogpu.sh

--- install_ai_tools/quick_flowise_ollama/ollama_wsl_nogpu.sh
+++ install_ai_tools/quick_flowise_ollama/ollama_wsl_nogpu.sh
@@ -583,6 +583,9 @@ if [ ! -f /home/$USER/flowise_installed.txt ]; then
 	   #ln -sT /home/$USER/start_flowise.sh start_flowise.sh
 	   ln -sT /home/$USER/stop_docker_flowise.sh stop_flowise.sh
 	   echo "flowise installed" > /home/$USER/flowise_installed.txt
+	   mkdir -p /home/$USER/Documents/flowise
+	   wget -c https://github.com/harnalashok/LLMs/blob/main/install_ai_tools/flowise/DesignChatflowsWithFlowise.pdf
+	   cd /home/$USER
 	   chmod +x /home/$USER/*.sh
 	   sleep 2
 	   wsl.exe --shutdown
