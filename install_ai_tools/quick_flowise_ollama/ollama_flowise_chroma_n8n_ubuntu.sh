e517d2202c67f251eeea8de6738ea76fbd0d9ed3
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-02-03 14:32:23 +0530

    Update ollama_flowise_chroma_n8n_ubuntu.sh

--- install_ai_tools/quick_flowise_ollama/ollama_flowise_chroma_n8n_ubuntu.sh
+++ install_ai_tools/quick_flowise_ollama/ollama_flowise_chroma_n8n_ubuntu.sh
@@ -1008,8 +1008,8 @@ if [ ! -f /home/$USER/postgresql_installed.txt ]; then
 		sudo -u postgres psql -c "\l"
 
 		# Finally change postgresql.conf and pg_hba.conf and make them highly permissive
-		version=$(psql -V | awk '{print $3}' |  cut -d '.' -f 1 | tr -d '\n')')
-        cd /etc/postgresql/$version/main
+		version=$(psql -V | awk '{print $3}' |  cut -d '.' -f 1 | tr -d '\n')
+		cd /etc/postgresql/$version/main
         echo "listen_addresses = '*'" |  sudo tee -a /etc/postgresql/$version/main/postgresql.conf
         echo "host    all             all             0.0.0.0/0               scram-sha-256" |  sudo tee -a /etc/postgresql/$version/main/pg_hba.conf
         sudo systemctl restart postgresql
