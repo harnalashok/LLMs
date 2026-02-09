41927fefa39d77522619729f55a59ea3ee10e279
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-02-09 15:21:52 +0530

    Update ollama_wsl_nogpu.sh

--- install_ai_tools/quick_flowise_ollama/ollama_wsl_nogpu.sh
+++ install_ai_tools/quick_flowise_ollama/ollama_wsl_nogpu.sh
@@ -799,6 +799,41 @@ if [ ! -f /home/$USER/postgresql_installed.txt ]; then
 	 fi  
  fi
 
+
+##########################
+### Install OpenNotebook
+# Ref: https://github.com/lfnovo/open-notebook/blob/main/docs/1-INSTALLATION/docker-compose.md
+##########################
+
+echo " "
+echo " "
+cd /home/$USER
+if [ ! -f /home/$USER/opennotebook_installed.txt ]; then
+    echo " "
+    echo " "
+	echo "------------"   
+	echo "Shall I install OpenNotebook docker? [Y,n]"    # 
+	read input
+	input=${input:-Y}
+	if [[ $input == "Y" || $input == "y" ]]; then
+	    echo " "
+	    echo " "
+		mkdir /home/$USER/opennotebook
+		cd /home/$USER/opennotebook
+		wget -Nc https://raw.githubusercontent.com/harnalashok/LLMs/refs/heads/main/install_ai_tools/opennotebook/docker-compose.yml
+		docker compose up -d
+		sleep 4
+		# Check health of system
+		curl http://localhost:5055/health
+		touch "opennotebook_installed.txt" > /home/$USER/opennotebook_installed.txt
+		cd /home/$USER
+		echo "Open browser to: http://localhost:8502"
+	else
+	    echo "OpenNotebook not installed"
+	fi
+fi
+
+
 ##########################
 ### Install RAGflow
 # Ref: https://github.com/infiniflow/ragflow
