e472954c6eb01ae3ada46b0cc8e0c9f9d6c0f744
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-02-17 11:55:37 +0530

    Update ollama_flowise_chroma_n8n_ubuntu.sh

--- install_ai_tools/quick_flowise_ollama/ollama_flowise_chroma_n8n_ubuntu.sh
+++ install_ai_tools/quick_flowise_ollama/ollama_flowise_chroma_n8n_ubuntu.sh
@@ -734,7 +734,7 @@ input=${input:-Y}
 		  #ollama list
 		  mkdir -p /home/$USER/Documents/huggingface
 		  cd /home/$USER/Documents/huggingface
-		  wget -c https://github.com/harnalashok/LLMs/blob/main/install_ai_tools/huggingface/Huggingface%20access%20token.pdf
+		  wget -c https://github.com/harnalashok/LLMs/blob/main/install_ai_tools/huggingface/huggingfaceAcessToken.pdf
 		  mv 'Huggingface access token.pdf' Huggingface_access_token.pdf
 		  cd /home/$USER/
 		  echo "models installed" > /home/$USER/models_installed.txt
