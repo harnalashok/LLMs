64a2b01a77852d68793c8668f3e6e55f731a09d7
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-03-15 18:14:38 +0530

    Update ollama_wsl_nogpu.sh

--- install_ai_tools/quick_flowise_ollama/ollama_wsl_nogpu.sh
+++ install_ai_tools/quick_flowise_ollama/ollama_wsl_nogpu.sh
@@ -1059,6 +1059,15 @@ if [ ! -f /home/$USER/postgresql_installed.txt ]; then
         echo "listen_addresses = '*'" |  sudo tee -a /etc/postgresql/$version/main/postgresql.conf
         echo "host    all             all             0.0.0.0/0               scram-sha-256" |  sudo tee -a /etc/postgresql/$version/main/pg_hba.conf
         sudo systemctl restart postgresql
+		# Download RAG data files
+		mkdir -p /home/$USER/Documents/data
+		cd /home/$USER/Documents/data
+		wget -Nc https://raw.githubusercontent.com/harnalashok/LLMs/refs/heads/main/install_ai_tools/essays/bertrandRusselEssays.txt
+		wget -Nc https://raw.githubusercontent.com/harnalashok/LLMs/refs/heads/main/install_ai_tools/essays/goodWriting.txt
+		wget -Nc https://raw.githubusercontent.com/harnalashok/LLMs/refs/heads/main/install_ai_tools/essays/iWorkedOnEssay.txt
+		wget -Nc https://raw.githubusercontent.com/harnalashok/LLMs/refs/heads/main/install_ai_tools/essays/sherlockHolmes.txt
+		wget -Nc https://raw.githubusercontent.com/harnalashok/LLMs/refs/heads/main/install_ai_tools/essays/slyFox.txt
+		wget -Nc https://raw.githubusercontent.com/harnalashok/LLMs/refs/heads/main/install_ai_tools/essays/threewishes.txt
         
 		cd /home/$USER
 		echo "postgresql installed" > /home/$USER/postgresql_installed.txt
@@ -1123,6 +1132,117 @@ if [ ! -f /home/$USER/faiss_installed.txt ]; then
 		echo "FAISS not installed"
 	fi	
 fi
+
+
+#################
+# langchain & langraph
+#################
+
+cd /home/$USER
+if [ ! -f /home/$USER/langchain_installed.txt ]; then
+    echo " "
+    echo " "
+	echo "Installing langchain and langraph.."
+    sleep 3
+	# Activate python environment at 'langchain'
+	#  for installing langchain and llama-index
+	##############
+	# Create python virtual env
+	##############
+	python3 -m venv /home/$USER/langchain
+	source /home/$USER/langchain/bin/activate
+	# 1.6 Essentials software
+	pip install spyder numpy scipy pandas matplotlib sympy cython
+	pip install jupyterlab
+	pip install ipython
+	pip install notebook
+	pip install streamlit
+	# Required for spyder:
+	sudo apt install pyqt5-dev-tools -y
+	# Huggingface and llama.cpp related
+	pip install huggingface_hub
+	# To connect to postgresql
+	pip install psycopg2
+	# Create script to activate 'langchain' env
+	echo "echo 'To activate langchain+llamaIndex virtual envs, activate as:' "  > /home/$USER/activate_langchain_venv.sh
+	echo "echo 'source /home/$USER/langchain/bin/activate' "                   >>  /home/$USER/activate_langchain_venv.sh
+	echo "echo '(Note the change in prompt after activating)' "                >>  /home/$USER/activate_langchain_venv.sh
+	echo "echo '(To deactivate, just enter the command: deactivate)' "         >>  /home/$USER/activate_langchain_venv.sh
+	echo "source /home/$USER/langchain/bin/activate"                           >>  /home/$USER/activate_langchain_venv.sh
+	chmod +x /home/$USER/*.sh
+	sleep 2
+	cp /home/$USER/activate_langchain_venv.sh  /home/$USER/start/activate_langchain_venv.sh
+	cp /home/$USER/activate_langchain_venv.sh  /home/$USER/stop/activate_langchain_venv.sh
+	source /home/$USER/langchain/bin/activate
+	pip install langchain
+	pip install langchain-openai
+	 pip install langchain-ollama
+	pip install langchain-community
+	pip install langchain-experimental
+	pip install langgraph
+	pip install "langserve[all]"
+	pip install langchain-cli
+	pip install unstructured
+	pip install unstructured[md]
+	#################
+	# llamaindex
+	# To be installed ONLY in langchain virtual env
+	#################
+	# 1.0 LLamaindex install
+	# Mostly openai related
+	echo "Installing llama-index"
+	echo "  "
+	pip install llama-index
+	# 1,1 Ollama, huggingface and localai (openailike) oriented
+	pip install --upgrade transformers
+	pip install llama-index-core llama-index-readers-file llama-index-llms-ollama llama-index-embeddings-ollama llama-index-embeddings-huggingface llama-index-llms-openai-like llama-index-vector-stores-faiss 
+	pip install llama-index-readers-file llama-index-embeddings-fastembed
+	# Needed inspite of code repeated above
+	pip install --upgrade transformers
+	# 1.2 Vector stores
+	pip install faiss-cpu
+	pip install qdrant-client llama-index-vector-stores-chroma 
+	pip install llama-index-vector-stores-qdrant fastembed
+	 pip install -U llama-index-vector-stores-postgres
+	# 1.3 Web access site
+	pip install tavily-python
+	# 1.4 Yahoo finance data
+	pip install yfinance
+	# 1.5 For groq, together, mistralAI access
+	pip install llama-index-llms-groq
+	pip install llama-index-llms-together
+	pip install llama-index-llms-mistralai
+	pip install llama-index-llms-openrouter
+	# Install deepeval evaluation system
+	pip install -U deepeval
+	# Download llamaindex tutorials
+	mkdir -p /home/$USER/Documents/llamaindex
+	cd /home/$USER/Documents/llamaindex
+	wget -nc https://raw.githubusercontent.com/harnalashok/LLMs/refs/heads/main/llamaindex/llamaindex_fundamentals.ipynb
+	wget -c https://raw.githubusercontent.com/harnalashok/LLMs/refs/heads/main/llamaindex/1_basic_agent.py
+	wget -nc https://raw.githubusercontent.com/harnalashok/LLMs/refs/heads/main/llamaindex/L0_simple_csv_moodle-expt.ipynb
+	wget -nc https://raw.githubusercontent.com/harnalashok/LLMs/refs/heads/main/llamaindex/L0_simple_skill_gap.ipynb
+	wget -nc https://raw.githubusercontent.com/harnalashok/LLMs/refs/heads/main/llamaindex/L1_Router_Engine.ipynb
+	wget -nc https://raw.githubusercontent.com/harnalashok/LLMs/refs/heads/main/llamaindex/L2_Tool_Calling.ipynb
+	wget -nc https://raw.githubusercontent.com/harnalashok/LLMs/refs/heads/main/llamaindex/L3_Building_an_Agent_Reasoning_Loop.ipynb
+	wget -nc https://raw.githubusercontent.com/harnalashok/LLMs/refs/heads/main/llamaindex/L4_Building_a_Multi-Document_Agent.ipynb
+	echo "langchain_installed.txt" > /home/$USER/langchain_installed.txt
+	sleep 3
+	chmod +x /home/$USER/*.sh
+	chmod +x /home/$USER/start/*.sh
+	chmod +x /home/$USER/stop/*.sh
+	LINE="  16. langchain and langgraph virtual env, 'langchain', created."
+	if ! grep -qF "$LINE" "$FILE"; then
+		echo "$LINE" >> "$FILE"
+	fi
+	sudo systemctl reboot -i
+else
+    echo "  "
+fi	
+
+
+
+
 
 
 ##########################
