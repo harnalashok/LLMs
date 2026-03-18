87ba5915b2c0a373e037fd52a6591a823aa19086
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-03-18 14:44:23 +0530

    Update simpleTable.sql

--- install_ai_tools/psql/simpleTable.sql
+++ install_ai_tools/psql/simpleTable.sql
@@ -56,51 +56,49 @@ CREATE TABLE ravi.public.spj (
   );
 
 
-  INSERT INTO ravi.public.s VALUES ("S1", 'Smith', 20, 'London');
-  INSERT INTO ravi.public.s VALUES ("S2", 'Jones', 10, 'Paris');
-  INSERT INTO ravi.public.s VALUES ("S3", 'Blake', 30, 'Paris');
-  INSERT INTO ravi.public.s VALUES ("S4", 'Clark', 20, 'London');
-  INSERT INTO ravi.public.s VALUES ("S5", 'Adams', 30, 'Athens');
+  INSERT INTO ravi.public.s (snum,sname,status,city) VALUES ('S1', 'Smith', 20, 'London');
+  INSERT INTO ravi.public.s VALUES ('S2', 'Jones', 10, 'Paris');
+  INSERT INTO ravi.public.s VALUES ('S3', 'Blake', 30, 'Paris');
+  INSERT INTO ravi.public.s VALUES ('S4', 'Clark', 20, 'London');
+  INSERT INTO ravi.public.s VALUES ('S5' , 'Adams', 30, 'Athens');
 
-  INSERT INTO p VALUES ("P1", 'Nut', 'Red', 12, 'London');
-  INSERT INTO p VALUES ("P2", 'Bolt', 'Green', 17, 'Paris');
-  INSERT INTO p VALUES ("P3", 'Screw', 'Blue', 17, 'Oslo');
-  INSERT INTO p VALUES ("P4", 'Screw', 'Red', 14, 'London');
-  INSERT INTO p VALUES ("P5", 'Cam', 'Blue', 12, 'Paris');
-  INSERT INTO p VALUES ("P6", 'Cog', 'Red', 19, 'London');
-  
-  
-  INSERT INTO j VALUES ("J1", 'Sorter', 'Paris');
-  INSERT INTO j VALUES ("J2", 'Display', 'Rome');
-  INSERT INTO j VALUES ("J3", 'OCR',  'Athens');
-  INSERT INTO j VALUES ("J4", 'Console','Athens');
-  INSERT INTO j VALUES ("J5", 'RAID',  'London');
-  INSERT INTO j VALUES ("J6", 'EDS',  'Oslo');
-  INSERT INTO j VALUES ("J7", 'Tape','London');
-  
-  
-  INSERT INTO spj VALUES("S1", "P1", "J1", 200) ;
-  INSERT INTO spj VALUES("S1", "P1", "J4", 700) ;
-  INSERT INTO spj VALUES("S2", "P3", "J1", 400) ;
-  INSERT INTO spj VALUES("S2", "P3", "J2", 200) ;
-  INSERT INTO spj VALUES("S2", "P3", "J3", 200) ;
-  INSERT INTO spj VALUES("S2", "P3", "J4", 500) ;
-  INSERT INTO spj VALUES("S2", "P3", "J5", 600) ;
-  INSERT INTO spj VALUES("S2", "P3", "J6", 400) ;
-  INSERT INTO spj VALUES("S2", "P3", "J7", 800) ;  
-  INSERT INTO spj VALUES("S2", "P5", "J2", 100) ;
-  INSERT INTO spj VALUES("S3", "P3", "J1", 200) ;
-  INSERT INTO spj VALUES("S3", "P4", "J2", 500) ;
-  INSERT INTO spj VALUES("S4", "P6", "J3", 300) ;
-  INSERT INTO spj VALUES("S4", "P6", "J7", 300) ;
-  INSERT INTO spj VALUES("S5", "P2", "J2", 200) ; 
-  INSERT INTO spj VALUES("S5", "P2", "J4", 100) ;
-  INSERT INTO spj VALUES("S5", "P5", "J5", 500) ;     
-  INSERT INTO spj VALUES("S5", "P5", "J7", 100) ;  
-  INSERT INTO spj VALUES("S5", "P6", "J2", 200) ;  
-  INSERT INTO spj VALUES("S5", "P1", "J4", 100) ;
-  INSERT INTO spj VALUES("S5", "P3", "J4", 200) ;
-  INSERT INTO spj VALUES("S5", "P4", "J4", 800) ;
+  INSERT INTO p VALUES ('P1', 'Nut', 'Red', 12, 'London');
+  INSERT INTO p VALUES ('P2' , 'Bolt', 'Green', 17, 'Paris');
+  INSERT INTO p VALUES ('P3', 'Screw', 'Blue', 17, 'Oslo');
+  INSERT INTO p VALUES ('P4', 'Screw', 'Red', 14, 'London');
+  INSERT INTO p VALUES ('P5' , 'Cam', 'Blue', 12, 'Paris');
+  INSERT INTO p VALUES ('P6', 'Cog', 'Red', 19, 'London');
+    
+  INSERT INTO j VALUES ('J1', 'Sorter', 'Paris');
+  INSERT INTO j VALUES ('J2', 'Display', 'Rome');
+  INSERT INTO j VALUES ('J3', 'OCR',  'Athens');
+  INSERT INTO j VALUES ('J4', 'Console','Athens');
+  INSERT INTO j VALUES ('J5', 'RAID',  'London');
+  INSERT INTO j VALUES ('J6', 'EDS',  'Oslo');
+  INSERT INTO j VALUES ('J7', 'Tape','London');
+   
+  INSERT INTO spj VALUES('S1', 'P1', 'J1', 200) ;
+  INSERT INTO spj VALUES('S1', 'P1', 'J4', 700) ;
+  INSERT INTO spj VALUES('S2', 'P3', 'J1', 400) ;
+  INSERT INTO spj VALUES('S2', 'P3', 'J2', 200) ;
+  INSERT INTO spj VALUES('S2', 'P3', 'J3', 200) ;
+  INSERT INTO spj VALUES('S2', 'P3', 'J4', 500) ;
+  INSERT INTO spj VALUES('S2', 'P3', 'J5', 600) ;
+  INSERT INTO spj VALUES('S2', 'P3', 'J6', 400) ;
+  INSERT INTO spj VALUES('S2', 'P3', 'J7', 800) ;  
+  INSERT INTO spj VALUES('S2', 'P5', 'J2', 100) ;
+  INSERT INTO spj VALUES('S3', 'P3', 'J1', 200) ;
+  INSERT INTO spj VALUES('S3', 'P4', 'J2', 500) ;
+  INSERT INTO spj VALUES('S4', 'P6', 'J3', 300) ;
+  INSERT INTO spj VALUES('S4', 'P6', 'J7', 300) ;
+  INSERT INTO spj VALUES('S5', 'P2', 'J2', 200) ; 
+  INSERT INTO spj VALUES('S5', 'P2', 'J4', 100) ;
+  INSERT INTO spj VALUES('S5', 'P5', 'J5', 500) ;     
+  INSERT INTO spj VALUES('S5', 'P5', 'J7', 100) ;  
+  INSERT INTO spj VALUES('S5', 'P6', 'J2', 200) ;  
+  INSERT INTO spj VALUES('S5', 'P1', 'J4', 100) ;
+  INSERT INTO spj VALUES('S5', 'P3', 'J4', 200) ;
+  INSERT INTO spj VALUES('S5', 'P4', 'J4', 800) ;
   INSERT INTO spj VALUES("S5", "P5", "J4", 400) ;
   INSERT INTO spj VALUES("S5", "P6", "J4", 500) ;
 
