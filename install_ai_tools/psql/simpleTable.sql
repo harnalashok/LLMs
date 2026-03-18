a0ddb777ac4dae9399af318e9000392ca8f3b74c
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-03-18 09:31:38 +0530

    Create simpleTable.sql

---
+++ install_ai_tools/psql/simpleTable.sql
@@ -0,0 +1,8 @@
+\c ravi ;
+CREATE TABLE ravi.public.distributors (
+                                           did  integer PRIMARY KEY,
+                                           name    varchar(40),
+                                           designation char(5)
+                                           );
+insert into ravi.public.distributors values (10, 'ashok', 'manag');
+insert into ravi.public.distributors values (100, 'vikas', 'dymg');
