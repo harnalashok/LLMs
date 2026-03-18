0c72fcff9fb6ce4100a63c5fe23bc5a228e61cfc
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-03-18 09:45:05 +0530

    Update simpleTable.sql

--- install_ai_tools/psql/simpleTable.sql
+++ install_ai_tools/psql/simpleTable.sql
@@ -1,8 +1,17 @@
 \c ravi ;
+drop table IF EXISTS ravi.public.distributors ;
 CREATE TABLE ravi.public.distributors (
                                            did  integer PRIMARY KEY,
                                            name    varchar(40),
-                                           designation char(5)
+                                           designation char(10)
                                            );
-insert into ravi.public.distributors values (10, 'ashok', 'manag');
-insert into ravi.public.distributors values (100, 'vikas', 'dymg');
+insert into ravi.public.distributors values (10, 'ashok', 'manager');
+insert into ravi.public.distributors values (100, 'vikas', 'dymgr');
+insert into ravi.public.distributors values (5, 'vidur', 'srmgr');
+insert into ravi.public.distributors values (15, 'kapil', 'dymgr');
+insert into ravi.public.distributors values (105, 'ashok', 'manager');
+insert into ravi.public.distributors values (110, 'vikas', 'manager');
+insert into ravi.public.distributors values (20, 'vidur', 'srmgr');
+insert into ravi.public.distributors values (30, 'kapil', 'dymgr');
+
+
