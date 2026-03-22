22ccae918a0f794b3fe769ffbde095b43f62cf47
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-03-22 10:14:05 +0530

    Update ollama_flowise_chroma_n8n_ubuntu.sh

--- install_ai_tools/quick_flowise_ollama/ollama_flowise_chroma_n8n_ubuntu.sh
+++ install_ai_tools/quick_flowise_ollama/ollama_flowise_chroma_n8n_ubuntu.sh
@@ -1096,6 +1096,7 @@ if [ ! -f /home/$USER/models_installed.txt ]; then
 	docker exec -it ollama ollama pull llama3.2:latest
 	docker exec -it ollama ollama pull llama3.2:1b
 	docker exec -it ollama ollama pull deepseek-r1:1.5b
+	docker exec -it ollama ollama pull qllama/bge-small-en-v1.5
 	docker exec -it ollama ollama pull gemma3:270m
 	docker exec -it ollama ollama pull qwen3.5:latest
 	docker exec -it ollama ollama pull qwen3:latest
