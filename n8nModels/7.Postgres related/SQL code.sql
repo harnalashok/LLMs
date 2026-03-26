0874882634094b900fae815cb4a98dbb48cd1c4f
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-03-26 20:41:46 +0530

    Add files via upload

--- n8nModels/7.Postgres related/SQL code.sql
+++ n8nModels/7.Postgres related/SQL code.sql
@@ -1,4 +1,15 @@
 --SQL code
+
+$sudo su kumar
+$psql kumar
+kumar=> \c kumar
+You are now connected to database "kumar" as user "kumar".
+kumar=> CREATE TABLE acars (  brand VARCHAR(255), model VARCHAR(255),year INT);
+kumar=> INSERT INTO acars (brand, model, year) VALUES ('Dzire','Maruti', 1994);
+kumar=> INSERT INTO acars (brand, model, year) VALUES ('Swift','Maruti', 1984);
+kumar=> select * from acars ;
+
+
 CREATE TABLE departments (
 		    		department_id int PRIMARY KEY,
 		    		department_name VARCHAR(100) NOT NULL
@@ -23,3 +34,46 @@ INSERT INTO employees (employee_id,employee_name, age, departmentId) values
 (2,'Bob',50.8, 10),
 (3,'sudha', 39.0, 20),
 (4,'Charlie',34.0, 30);
+
+Get DB Schema
+=============
+{{ $fromAI("sql_query", "SQL Query") }}
+
+SELECT 
+    table_schema,
+    table_name
+FROM information_schema.tables
+WHERE table_type = 'BASE TABLE'
+    AND table_schema NOT IN ('pg_catalog', 'information_schema')
+ORDER BY table_schema, table_name;
+
+Get Table Definition
+==================
+
+select
+  c.column_name,
+  c.data_type,
+  c.is_nullable,
+  c.column_default,
+  tc.constraint_type,
+  ccu.table_name AS referenced_table,
+  ccu.column_name AS referenced_column
+from
+  information_schema.columns c
+LEFT join
+  information_schema.key_column_usage kcu
+  ON c.table_name = kcu.table_name
+  AND c.column_name = kcu.column_name
+LEFT join
+  information_schema.table_constraints tc
+  ON kcu.constraint_name = tc.constraint_name
+  AND tc.constraint_type = 'FOREIGN KEY'
+LEFT join
+  information_schema.constraint_column_usage ccu
+  ON tc.constraint_name = ccu.constraint_name
+where
+  c.table_name = '{{ $fromAI("table_name") }}'
+  AND c.table_schema = '{{ $fromAI("schema_name") }}'
+order by
+  c.ordinal_position
+
