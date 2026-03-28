1134a58463a1d6b08336715df2c746918ad47f12
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-03-28 11:13:42 +0530

    Update ollama_wsl_nogpu.sh

--- install_ai_tools/quick_flowise_ollama/ollama_wsl_nogpu.sh
+++ install_ai_tools/quick_flowise_ollama/ollama_wsl_nogpu.sh
@@ -674,8 +674,8 @@ cd /home/$USER
 		# Finally change postgresql.conf and pg_hba.conf and make them highly permissive
 		version=$(psql -V | awk '{print $3}' |  cut -d '.' -f 1 | tr -d '\n')
 		cd /etc/postgresql/$version/main
-        echo "listen_addresses = '*'" |  sudo tee -a /etc/postgresql/$version/main/postgresql.conf
-        echo "host    all             all             0.0.0.0/0               scram-sha-256" |  sudo tee -a /etc/postgresql/$version/main/pg_hba.conf
+        echo "listen_addresses = '*'" | sudo tee -a /etc/postgresql/$version/main/postgresql.conf
+        echo "host    all             all             0.0.0.0/0               scram-sha-256" | sudo tee -a /etc/postgresql/$version/main/pg_hba.conf
         sudo systemctl restart postgresql
 		# Download RAG data files
 		mkdir -p /home/$USER/Documents/data
