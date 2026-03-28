8ea89717fa5aa2d75ea8ac9a2ea457cf4bcc9f4f
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-03-28 10:31:15 +0530

    Update ollama_wsl_gpu.sh

--- install_ai_tools/quick_flowise_ollama/ollama_wsl_gpu.sh
+++ install_ai_tools/quick_flowise_ollama/ollama_wsl_gpu.sh
@@ -196,7 +196,7 @@ if [ ! -f /home/$USER/cuda_installed.txt ]; then
 			#sudo dpkg -i cuda-repo-wsl-ubuntu-13-0-local_13.0.1-1_amd64.deb
 			#sudo cp /var/cuda-repo-wsl-ubuntu-13-0-local/cuda-*-keyring.gpg /usr/share/keyrings/
 			#sudo apt-get -y install cuda-toolkit-13-0
-			wget https://developer.download.nvidia.com/compute/cuda/13.2.0/local_installers/cuda-repo-wsl-ubuntu-13-2-local_13.2.0-1_amd64.deb
+			wget -c https://developer.download.nvidia.com/compute/cuda/13.2.0/local_installers/cuda-repo-wsl-ubuntu-13-2-local_13.2.0-1_amd64.deb
             sudo dpkg -i cuda-repo-wsl-ubuntu-13-2-local_13.2.0-1_amd64.deb
             sudo cp /var/cuda-repo-wsl-ubuntu-13-2-local/cuda-*-keyring.gpg /usr/share/keyrings/
             sudo apt-get update
