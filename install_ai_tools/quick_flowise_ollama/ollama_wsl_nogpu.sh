8c8d31ff65c6279fe2cc35cf4b4cee0c79a9246d
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-04-12 16:40:59 +0530

    Update ollama_wsl_nogpu.sh

--- install_ai_tools/quick_flowise_ollama/ollama_wsl_nogpu.sh
+++ install_ai_tools/quick_flowise_ollama/ollama_wsl_nogpu.sh
@@ -1191,6 +1191,7 @@ if [ ! -f /home/$USER/smoll_installed.txt ]; then
 	pip install "smolagents[toolkit]"
 	pip install "smolagents[mcp]"
 	pip install "smolagents[litellm]"		
+	pip install 'smolagents[transformers]'
 	pip install -U "huggingface_hub[cli]"
 	pip install -U huggingface_hub
 	pip install ddgs
