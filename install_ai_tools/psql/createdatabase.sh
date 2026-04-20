6546b61990a7b907938133ea779a4d8b93a7cce2
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-04-20 10:59:12 +0530

    Update createdatabase.sh

--- install_ai_tools/psql/createdatabase.sh
+++ install_ai_tools/psql/createdatabase.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Last amended: 20th April, 2026
+
 echo "================"
-echo "Will create a user and database of the same name"
+echo "Will create an ubuntu & postgresql user and database of the same name"
 echo "And also make it vectorized database"
 echo "================"
 sleep 4
@@ -9,6 +11,7 @@ echo "   "
 echo "   "
 read -p "What user/database name would you like to have: " name
 echo $name
+sudo useradd -m -s /bin/bash $name
 sudo -u postgres psql -c 'create user "'"$name"'" ;'
 sudo -u postgres psql -c 'CREATE DATABASE "'"$name"'" WITH OWNER = "'"$name"'";  '
 sudo -u postgres psql -c 'grant all privileges on database "'"$name"'" to "'"$name"'";'
