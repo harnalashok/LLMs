638cfba47aed7480d6c5e725adcc1baee15681eb
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-04-23 14:12:33 +0530

    changed version of ragflow

--- install_ai_tools/quick_flowise_ollama/install_ubuntu.sh
+++ install_ai_tools/quick_flowise_ollama/install_ubuntu.sh
@@ -2217,14 +2217,14 @@ if [  -f /home/$USER/ragflow_installed.txt ]; then
 			cd /home/$USER/ragflow
 			# https://ragflow.io/docs/dev/upgrade_ragflow#upgrade-ragflow-to-the-most-recent-officially-published-release
 			# Switch to the latest, officially published release, e.g., v0.23.1:
-			echo "3.0 Will upgrade to ver 0.23.1"
+			echo "3.0 Will upgrade to ver 0.25.0"
 			sleep 5
 			# Switch working directory of git
-			git checkout v0.23.1
+			git checkout v0.25.0
 			#
 			# Update ragflow/docker/.env:
 			cd /home/$USER/ragflow/docker
-		    RAGFLOW_IMAGE=infiniflow/ragflow:v0.23.1
+		    RAGFLOW_IMAGE=infiniflow/ragflow:v0.25.0
 			sed -i 's/SVR_WEB_HTTP_PORT=80/SVR_WEB_HTTP_PORT=800/' .env
 			sed -i 's/SVR_WEB_HTTPS_PORT=443/SVR_WEB_HTTPS_PORT=1443/' .env
 			#
