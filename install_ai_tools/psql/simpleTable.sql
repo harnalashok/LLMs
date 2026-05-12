c677decb135e06b49b76c5738a56161006ba920a
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-05-12 10:39:51 +0530

    Update simpleTable.sql

--- install_ai_tools/psql/simpleTable.sql
+++ install_ai_tools/psql/simpleTable.sql
@@ -101,9 +101,9 @@ CREATE TABLE ravi.public.spj (
   INSERT INTO spj VALUES('S5', 'P5', 'J4', 400) ;
   INSERT INTO spj VALUES('S5', 'P6', 'J4', 500) ;
 
-
-#select * from s ;
-#select * from p ;
-#select * from j ;
-#select * from spj ;
-
+/*
+select * from s ;
+select * from p ;
+select * from j ;
+select * from spj ;
+*/
