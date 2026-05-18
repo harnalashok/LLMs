285672526d3cf10d1eea56cfed0edcccbc9c4556
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-05-18 09:29:48 +0530

    Added modelk

--- install_ai_tools/quick_flowise_ollama/install_ubuntu.sh
+++ install_ai_tools/quick_flowise_ollama/install_ubuntu.sh
@@ -1182,6 +1182,7 @@ if [ ! -f /home/$USER/models_installed.txt ]; then
 	docker exec -it ollama ollama pull qwen3.5:0.8b
 	docker exec -it ollama ollama pull openhermes
 	docker exec -it ollama ollama pull openchat:latest
+	docker exec -it ollama ollama pull NeuralNet/openchat-3.6
 	echo " "
 	echo " "
 	#ollama list
