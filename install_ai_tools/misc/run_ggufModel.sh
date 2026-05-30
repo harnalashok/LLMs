fc05c8646fd525d726b560ac5ba7dda704d5a837
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-05-30 15:57:35 +0530

    Update run_ggufModel.sh

--- install_ai_tools/misc/run_ggufModel.sh
+++ install_ai_tools/misc/run_ggufModel.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
+# Download PsycoLLM.Q4_K_M.gguf or any other gguf folder
+#  and place it in /home/$USER/gguf_models/ :
 
+# No space before and after '='
+modelFolder=/home/$USER/gguf_models/
 modelName="PsycoLLM.Q4_K_M.gguf"
 
 
-###################
+######## Do NOT AMEND CODE BELOW ###########
 
 echo "  "
 echo "  "
 echo "====="
 echo "Would execute llama.cpp"
-echo "Default model folder: /home/$USER/gguf_models"
+echo "Default gguf model folder: $modelFolder"
 echo "Default model: $modelName"
-echo "Download any gguf models and place them in folder gguf_models"
+echo "Download any gguf model and first place it "
+echo "  in folder gguf_models."
 echo "======"
-sleep 5
-
-modelName="PsycoLLM.Q4_K_M.gguf"
+sleep 3
 
 # For CPU
 
 llama-cli \
-  -m /home/$USER/gguf_models/$modelName \
+  -m $modelFolder$modelName \
   -p "Generate a psychometric test for an introvert person. To test this trait, generate ten questions." \
   -sys "You are an expert psychometric test generator" \
   -ngl 0 \
