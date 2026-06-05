5014b43bda51a1a9ec48382bb9084946299907b9
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-06-05 16:34:49 +0530

    Updating cuda

--- install_ai_tools/quick_flowise_ollama/ollama_wsl_gpu.sh
+++ install_ai_tools/quick_flowise_ollama/ollama_wsl_gpu.sh
@@ -228,11 +228,11 @@ if [ ! -f /home/$USER/cuda_installed.txt ]; then
 			#sudo dpkg -i cuda-repo-wsl-ubuntu-13-0-local_13.0.1-1_amd64.deb
 			#sudo cp /var/cuda-repo-wsl-ubuntu-13-0-local/cuda-*-keyring.gpg /usr/share/keyrings/
 			#sudo apt-get -y install cuda-toolkit-13-0
-			wget -c https://developer.download.nvidia.com/compute/cuda/13.2.0/local_installers/cuda-repo-wsl-ubuntu-13-2-local_13.2.0-1_amd64.deb
-            sudo dpkg -i cuda-repo-wsl-ubuntu-13-2-local_13.2.0-1_amd64.deb
-            sudo cp /var/cuda-repo-wsl-ubuntu-13-2-local/cuda-*-keyring.gpg /usr/share/keyrings/
+			wget -c https://developer.download.nvidia.com/compute/cuda/13.3.0/local_installers/cuda-repo-wsl-ubuntu-13-3-local_13.3.0-1_amd64.deb
+			sudo dpkg -i cuda-repo-wsl-ubuntu-13-3-local_13.3.0-1_amd64.deb
+			sudo cp /var/cuda-repo-wsl-ubuntu-13-3-local/cuda-*-keyring.gpg /usr/share/keyrings/
             sudo apt-get update
-            sudo apt-get -y install cuda-toolkit-13-2
+            sudo apt-get -y install cuda-toolkit-13-3
             sudo apt autoremove -y
             echo "Which NVIDIA driver I have:"
             echo "============================"
