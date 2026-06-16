65ff836c123bff1d86cc99ca4471e7b445b4fb78
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-06-16 10:05:10 +0530

    Update test_laptop_gpu.sh

--- install_ai_tools/quick_flowise_ollama/test_laptop_gpu.sh
+++ install_ai_tools/quick_flowise_ollama/test_laptop_gpu.sh
@@ -9,23 +9,22 @@ rm /home/$USER/test_report.txt
 # Stop software, if started
 echo "Stopping services, if started..."
 echo "========="
-
+sleep 2
 
 nn=
 nn=`sudo ss -tulpn | grep ':5678'`
 
 if [[ -n $nn ]]; then
-echo "Stopping n8n"
-docker stop n8n
+  echo "Stopping n8n"
+  docker stop n8n
 fi
-
 
 ol=
 ol=`sudo ss -tulpn | grep ':11434'`
 
 if [[ -n $ol ]]; then
-echo "Stopping ollama docker"
-docker stop ollama
+  echo "Stopping ollama docker"
+  docker stop ollama
 fi
 
 
@@ -33,8 +32,8 @@ chr=
 chr=`sudo ss -tulpn | grep ':8000'`
 
 if [[ -n $chr ]]; then
-echo "Stopping chromadb"
-docker stop chroma
+  echo "Stopping chromadb"
+  docker stop chroma
 fi
 
 
@@ -42,8 +41,8 @@ flo=
 flo=`sudo ss -tulpn | grep ':3000'`
 
 if [[ -n $flo ]]; then
-echo "Stopping flowise"
-docker stop flowise
+  echo "Stopping flowise"
+  docker stop flowise
 fi
 
 echo "   "
@@ -56,109 +55,96 @@ echo "   "
 echo "    "
 
 # Start apps
-echo "  "
+echo "   "
+echo "======  "
 echo "Starting flowise   "
 bash start_flowise.sh
 sleep 3
 echo "  "
+echo "======  "
 echo "Starting Ollama   "
 bash start_ollama.sh
 sleep 3
 echo "  "
+echo "======  "
 echo "Starting n8n   "
 bash start_n8n.sh
 sleep 3
 echo "  "
+echo "======  "
 echo "Starting Chroma   "
 bash start_chroma.sh
 echo "  "
+echo "======  "
 echo "Starting meilisearch   "
 bash start_meilisearch.sh 
 sleep 3
 echo "  "
+echo "======  "
 echo "Starting Postgres   "
 bash start_postgresql.sh
 sleep 3
 
-echo "=============="
 echo "  "
 echo "   "
-#######3
-
-echo "======="         > /home/$USER/test_report.txt
-echo "Test Report"     >> /home/$USER/test_report.txt
-echo "======="         >> /home/$USER/test_report.txt
-echo "    "            >> /home/$USER/test_report.txt
+echo "======="                 > /home/$USER/test_report.txt
+echo "Writing Test Report"     >> /home/$USER/test_report.txt
+echo "======="                 >> /home/$USER/test_report.txt
+echo "    "                    >> /home/$USER/test_report.txt
 
 
-abc=
-abc=`sudo ss -tulpn | grep ':5678'`
-
-if [[ -n $abc ]]; then
+nn=
+nn=`sudo ss -tulpn | grep ':5678'`
+if [[ -n $nn ]]; then
   echo "1. n8n is started."  >> /home/$USER/test_report.txt
 else
    echo "1. n8n NOT started"  >> /home/$USER/test_report.txt
 fi
 
 # Test ollama
-
-abc=
-abc=`sudo ss -tulpn | grep ':11434'`
-
-if [[ -n $abc ]]; then
+ol=
+ol=`sudo ss -tulpn | grep ':11434'`
+if [[ -n $ol ]]; then
   echo "2. ollama is started."    >> /home/$USER/test_report.txt
 else
    echo "2. ollama NOT started"  >> /home/$USER/test_report.txt
 fi
 
-#######3
 # Test postgres
-
-
-abc=
-abc=`sudo ss -tulpn | grep ':5432'`
-
-if [[ -n $abc ]]; then
+pg=
+pg=`sudo ss -tulpn | grep ':5432'`
+if [[ -n $pg ]]; then
   echo "3. postgres is started."    >> /home/$USER/test_report.txt
 else
    echo "3. postgres NOT started"    >> /home/$USER/test_report.txt
 fi
 
-#######3
-abc=
-abc=`sudo ss -tulpn | grep ':8000'`
 
-if [[ -n $abc ]]; then
+chr=
+chr=`sudo ss -tulpn | grep ':8000'`
+
+if [[ -n $chr ]]; then
   echo "4. chromadb is started."     >> /home/$USER/test_report.txt
 else
    echo "4. chromadb NOT started"    >> /home/$USER/test_report.txt
 fi
 
-#######3
-
-abc=
-abc=`sudo ss -tulpn | grep ':7700'`
-
-if [[ -n $abc ]]; then
+mei=
+mei=`sudo ss -tulpn | grep ':7700'`
+if [[ -n $mei ]]; then
   echo "5. meilisearch is started."     >> /home/$USER/test_report.txt
 else
    echo "5. meilisearch NOT started"   >> /home/$USER/test_report.txt
 fi
 
-###########
-
 sleep 3
-
-acc=
-acc=`sudo ss -tulpn | grep ':3000'`
-
-if [[ -n $acc ]]; then
+flo=
+flo=`sudo ss -tulpn | grep ':3000'`
+if [[ -n $flo ]]; then
   echo "6. flowise is started."     >> /home/$USER/test_report.txt
 else
    echo "6. flowise is NOT started"    >> /home/$USER/test_report.txt
 fi
-
-###########
 
 abc=
 abc=`nvidia-smi | grep 'Memory-Usage'`
@@ -167,8 +153,6 @@ if [[ -n $abc ]]; then
 else
    echo "7. cuda NOT available"    >> /home/$USER/test_report.txt
 fi
-
-###############3
 
 abc=
 abc=`node --version | grep '22'`
@@ -189,7 +173,7 @@ fi
 sleep 2
 
 #echo "======"     >> /home/$USER/test_report.txt
-ollama list       | tee -a  /home/$USER/test_report.txt
+docker exec -it ollama ollama list       | tee -a  /home/$USER/test_report.txt
 echo "======"     >> /home/$USER/test_report.txt
 
 cat /home/$USER/test_report.txt
