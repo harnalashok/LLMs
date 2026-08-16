b66bb81225957e72b3d3b587b8c2c38b08667bfe
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-08-16 06:54:35 +0530

    Update ollama_wsl_nogpu.sh

--- install_ai_tools/quick_flowise_ollama/ollama_wsl_nogpu.sh
+++ install_ai_tools/quick_flowise_ollama/ollama_wsl_nogpu.sh
@@ -1499,6 +1499,10 @@ if [ ! -f /home/$USER/llamaindexExamples_installed.txt ]; then
 	mv /home/$USER/Documents/llamaindexExamples/llamaindex/* .
 	rm -rf /home/$USER/Documents/llamaindexExamples
 	cd /home/$USER
+	rm -rf /home/$USER/lprojects
+	mkdir /home/$USER/lprojects
+	cp /home/$USER/Documents/llamaindex/revised14042026/*.pdf    /home/$USER/lprojects
+	cp /home/$USER/Documents/llamaindex/revised14042026/*.ipynb  /home/$USER/lprojects
 	echo "llamaindexExamples_installed.txt" > /home/$USER/llamaindexExamples_installed.txt
 else
 	echo "  "
