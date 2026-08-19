7be2c0ce9a7c0cda2f9d51e3befc8bb2c2ca18b1
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-08-19 11:22:36 +0530

    amended llamaindexExamples

--- install_ai_tools/quick_flowise_ollama/ollama_wsl_nogpu.sh
+++ install_ai_tools/quick_flowise_ollama/ollama_wsl_nogpu.sh
@@ -1576,17 +1576,27 @@ if [ ! -f /home/$USER/llamaindexExamples_installed.txt ]; then
 
 	echo "Populating lprojects"
 	cd /home/$USER
+	
 	rm -rf /home/$USER/lprojects
 	mkdir /home/$USER/lprojects
+	mkdir /home/$USER/lprojects/txt_files
 	mkdir /home/$USER/lprojects/md_files
-	cp /home/$USER/Documents/llamaindex/data/*.pdf         /home/$USER/lprojects/
-	cp /home/$USER/Documents/llamaindex/data/*.csv         /home/$USER/lprojects/
-	cp /home/$USER/Documents/llamaindex/revised14042026/txt_files/* /home/$USER/lprojects/
-	cp /home/$USER/Documents/llamaindex/revised14042026/*  /home/$USER/lprojects/
-	cp /home/$USER/Documents/llamaindex/data/metagpt.pdf   /home/$USER/lprojects/
-	cp /home/$USER/Documents/llamaindex/data/sports.pdf    /home/$USER/lprojects/
-    cp /home/$USER/Documents/llamaindex/data/customers-100.csv	 /home/$USER/lprojects/
-	cp /home/ashok/Documents/llamaindex/data/md_files/*.md       /home/$USER/lprojects/md_files/
+	mkdir /home/$USER/lprojects/md_files1
+	mkdir /home/$USER/lprojects/pg
+	mkdir /home/$USER/lprojects/pdf
+	mkdir /home/$USER/lprojects/csv
+
+	cp /home/$USER/Documents/llamaindex/data/*.pdf                   /home/$USER/lprojects/pdf
+	cp /home/$USER/Documents/llamaindex/data/*.csv                   /home/$USER/lprojects/csv
+	cp -r /home/$USER/Documents/llamaindex/revised14042026/txt_files/*  /home/$USER/lprojects/txt_files/
+	cp /home/$USER/Documents/llamaindex/revised14042026/*            /home/$USER/lprojects/
+	cp /home/$USER/Documents/llamaindex/data/customers-100.csv	     /home/$USER/lprojects/
+	cp /home/$USER/Documents/llamaindex/data/md_files/*.md           /home/$USER/lprojects/md_files/
+	cp /home/$USER/Documents/llamaindex/postgresql/*.md              /home/$USER/lprojects/md_files1/
+	cp /home/$USER/Documents/llamaindex/postgresql/*.ipynb           /home/$USER/lprojects/pg
+	cp /home/$USER/Documents/llamaindex/postgresql_SQL/*.py          /home/$USER/lprojects/pg 
+
+
 	echo "llamaindexExamples_installed.txt" > /home/$USER/llamaindexExamples_installed.txt
 else
 	echo "  "
