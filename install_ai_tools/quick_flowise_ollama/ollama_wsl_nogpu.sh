1dda6f119a2bb99123c741196f6eb8d79827b6cd
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-08-20 11:26:26 +0530

    amended hard_reset.sh

--- install_ai_tools/quick_flowise_ollama/ollama_wsl_nogpu.sh
+++ install_ai_tools/quick_flowise_ollama/ollama_wsl_nogpu.sh
@@ -921,7 +921,7 @@ if [ ! -f /home/$USER/n8n_installed.txt ]; then
 					docker.n8n.io/n8nio/n8n
 
     echo $password | sudo chown -R 1000:1000 /home/$USER/.n8n
-		
+    IP_ADDRESS=$(hostname -I | awk '{print $1}')		
 	# n8n start script for Ubuntu
 	echo '#!/bin/bash'                                                                                                        > /home/$USER/start_n8n.sh
 	echo " "                                                                                                                  >> /home/$USER/start_n8n.sh
@@ -934,7 +934,7 @@ if [ ! -f /home/$USER/n8n_installed.txt ]; then
 	echo "echo '6. Memory usage: Use \"top -u $USER\" OR \"free -g \" '"                                                      >>  /home/$USER/start_n8n.sh
 	echo "sleep 4"                                                                                                             >> /home/$USER/start_n8n.sh
 	#echo "docker run -it -d --rm --name n8n -p 5678:5678 -e WEBHOOK_URL=\"http://${IP_ADDRESS}:5678/\" -e NODE_OPTIONS=\"--max-old-space-size=4096\" --network host -v n8n_data:/home/node/.n8n docker.n8n.io/n8nio/n8n"   >> /home/$USER/start_n8n.sh
-	echo "docker run -it -d --rm --name n8n -p 5678:5678 -e WEBHOOK_URL="http://192.240.4.114:5678/" -e NODE_OPTIONS="--max-old-space-size=4096" --network host -v /home/ashok/.n8n:/home/node/.n8n docker.n8n.io/n8nio/n8n"   >> /home/$USER/start_n8n.sh
+	echo "docker run -it -d --rm --name n8n -p 5678:5678 -e WEBHOOK_URL=\"http://$IP_ADDRESS:5678/\" -e NODE_OPTIONS=\"--max-old-space-size=4096\" --network host -v /home/ashok/.n8n:/home/node/.n8n docker.n8n.io/n8nio/n8n"   >> /home/$USER/start_n8n.sh
 	echo "echo '  '"                                              >> /home/$USER/start_n8n.sh
 	echo "echo '======NOTE==='"                                   >> /home/$USER/start_n8n.sh
 	echo "echo 'if n8n does not get started, issue command:'"     >> /home/$USER/start_n8n.sh
@@ -959,6 +959,32 @@ if [ ! -f /home/$USER/n8n_installed.txt ]; then
     echo "echo '   Last Name:   harnal'"                       >> /home/$USER/reset_n8n.sh
     echo "echo '   password:   Ashok@12345'"                   >> /home/$USER/reset_n8n.sh
     echo "echo '==**====**====='"                              >> /home/$USER/reset_n8n.sh
+	
+	# Hard Reset n8n password
+    echo '#!/bin/bash'                                          > /home/$USER/hard_reset_n8n.sh
+    echo " "                                                   >> /home/$USER/hard_reset_n8n.sh
+	echo "echo 'Stopping n8n, if started'"                     >> /home/$USER/hard_reset_n8n.sh
+    echo "docker stop n8n"                                     >> /home/$USER/hard_reset_n8n.sh
+    echo "sleep 3"                                             >> /home/$USER/hard_reset_n8n.sh
+	echo "sudo rm -rf /home/$USER/.n8n"						   >> /home/$USER/hard_reset_n8n.sh
+	echo "echo 'Restarting n8n'"                               >> /home/$USER/hard_reset_n8n.sh
+	echo "bash start_n8n.sh"                                   >> /home/$USER/hard_reset_n8n.sh
+	echo "sleep 3"                                             >> /home/$USER/hard_reset_n8n.sh
+	echo "sudo chown -R 1000:1000 /home/$USER/.n8n"            >> /home/$USER/hard_reset_n8n.sh
+	echo "echo 'Done...'"                                      >> /home/$USER/hard_reset_n8n.sh
+	echo "sleep 3"                                             >> /home/$USER/hard_reset_n8n.sh
+	echo "echo 'Starting n8n again...'"                        >> /home/$USER/hard_reset_n8n.sh
+	echo "bash start_n8n.sh"   >> /home/$USER/hard_reset_n8n.sh
+    echo "echo '==**====**====='"                              >> /home/$USER/hard_reset_n8n.sh
+    echo "echo 'For uniformity, keep details as follows:'"     >> /home/$USER/hard_reset_n8n.sh
+    echo "echo '   email:       ashok@fsm.ac.in'"              >> /home/$USER/hard_reset_n8n.sh
+    echo "echo '   First Name:  ashok'"                        >> /home/$USER/hard_reset_n8n.sh
+    echo "echo '   Last Name:   harnal'"                       >> /home/$USER/hard_reset_n8n.sh
+    echo "echo '   password:   Ashok@12345'"                   >> /home/$USER/hard_reset_n8n.sh
+    echo "echo '==**====**====='"                              >> /home/$USER/hard_reset_n8n.sh
+	echo "sleep 3"                                             >> /home/$USER/hard_reset_n8n.sh
+	echo "netstat -aunt | grep 5678"                           >> /home/$USER/hard_reset_n8n.sh
+
 	# update n8n
     echo '#!/bin/bash'                                          >  /home/$USER/update_n8n.sh
     echo " "                                                   >> /home/$USER/update_n8n.sh
