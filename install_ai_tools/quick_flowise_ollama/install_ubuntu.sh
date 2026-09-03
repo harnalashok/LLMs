4084b3d6674ecce39275c48da38d07225f8e271b
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-09-03 11:04:56 +0530

    minor change

--- install_ai_tools/quick_flowise_ollama/install_ubuntu.sh
+++ install_ai_tools/quick_flowise_ollama/install_ubuntu.sh
@@ -1926,6 +1926,8 @@ if [ ! -f /home/$USER/llamaindexExamples_installed.txt ]; then
 	mkdir /home/$USER/lprojects/pg
 	mkdir /home/$USER/lprojects/pdf
 	mkdir /home/$USER/lprojects/csv
+	mkdir /home/$USER/lprojects/md_files_sample
+
 	cp /home/$USER/Documents/llamaindex/data/*.pdf                   /home/$USER/lprojects/pdf
 	cp /home/$USER/Documents/llamaindex/data/*.csv                   /home/$USER/lprojects/csv
 	cp -r /home/$USER/Documents/llamaindex/revised14042026/txt_files/*  /home/$USER/lprojects/txt_files/
@@ -1935,6 +1937,10 @@ if [ ! -f /home/$USER/llamaindexExamples_installed.txt ]; then
 	cp /home/$USER/Documents/llamaindex/postgresql/*.md              /home/$USER/lprojects/md_files1/
 	cp /home/$USER/Documents/llamaindex/postgresql/*.ipynb           /home/$USER/lprojects/pg
 	cp /home/$USER/Documents/llamaindex/postgresql_SQL/*.py          /home/$USER/lprojects/pg 
+	cd /home/$USER/lprojects/md_files/
+	cp SCorder1.md  Scorder2.md  SCorder4.md                         /home/$USER/lprojects/md_files_sample/
+	cd /home/$USER
+	
 	#rm -rf /home/$USER/lprojects
 	#mkdir -p /home/$USER/lprojects/md_files
 	#cp /home/$USER/Documents/llamaindex/revised14042026/*.pdf    /home/$USER/lprojects
