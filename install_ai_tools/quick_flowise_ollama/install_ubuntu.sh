fc918ca3103e52b1cd8f12f5e19bf0dff8aedde7
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-09-07 11:46:53 +0530

    Commented out milvus

--- install_ai_tools/quick_flowise_ollama/install_ubuntu.sh
+++ install_ai_tools/quick_flowise_ollama/install_ubuntu.sh
@@ -11,7 +11,7 @@ password="ashok"
 
 # echo "185.199.111.133 raw.githubusercontent.com" | sudo tee -a /etc/hosts
 # echo $password | sudo -S sh -c 'echo "185.199.111.133 raw.githubusercontent.com" >> /etc/hosts'
-
+echo $password | sudo -S sh -c 'echo "185.199.111.133 raw.githubusercontent.com" >> /etc/hosts'
 echo "========script=============="
 echo "Will update Ubuntu and also install nodeJS"
 echo "Will install cuda toolkit"
@@ -918,75 +918,75 @@ if [ ! -f /home/$USER/vectordb_installed.txt ]; then
 	# Webui avaiable at: http://localhost:9091/webui
 	# Ref: https://milvus.io/docs/install_standalone-docker.md
 	################
-	echo "  "
-	echo "   "
-	cd /home/$USER/
-	echo "====  "    
-	echo "Installing milvus vector database using docker"       
-	echo "You may be asked for the password. Supply it..."     
-	echo "====  "                                                   
-	sleep 3
-	echo $password | sudo -S  apt-get update
-	curl -sfL https://raw.githubusercontent.com/milvus-io/milvus/master/scripts/standalone_embed.sh -o standalone_embed.sh
-	bash standalone_embed.sh start
-	echo " "
-	echo "Milvus vector database installed"                      
-	echo "Ports used are: 9091 and 19530."                       
-	echo "To restart/stop docker use the following commands:"            
-	echo "     sudo bash standalone_embed.sh restart|start|stop|upgrade|delete"                      
-	mkdir /home/$USER/milvus
-	mv standalone_embed.sh /home/$USER/milvus/
-	echo 'export PATH="$PATH:/home/$USER/milvus/"' >> /home/$USER/.bashrc
-	# Our milvus start script		
-	echo '#!/bin/bash'                                         | tee    /home/$USER/start/start_milvus.sh
-	echo " "                                                   | tee -a /home/$USER/start/start_milvus.sh
-	echo "cd ~/"                                               | tee -a /home/$USER/start/start_milvus.sh
-	echo "echo 'Ports are: 9091 and 19530.'"                   | tee -a /home/$USER/start/start_milvus.sh
-	echo "echo 'Data is in /home/$USER/volumes/milvus/'"                   | tee -a /home/$USER/start/start_milvus.sh
-	echo "echo 'Access in flowise as: http://<hostIP>:19530.'"           | tee -a /home/$USER/start/start_milvus.sh
-	echo "cd /home/$USER/milvus"                               | tee -a /home/$USER/start/start_milvus.sh
-	echo "bash standalone_embed.sh start"                      | tee -a /home/$USER/start/start_milvus.sh
-	echo "cd /home/$USER"                                       | tee -a /home/$USER/start/start_milvus.sh 
-	echo "netstat -aunt | grep 19530"                          | tee -a /home/$USER/start/start_milvus.sh
-	# Stop script		
-	echo '#!/bin/bash'                                         | tee    /home/$USER/stop/stop_milvus.sh 
-	echo " "                                                   | tee -a /home/$USER/stop/stop_milvus.sh 
-	echo "cd ~/"                                               | tee -a /home/$USER/stop/stop_milvus.sh 
-	echo "cd /home/$USER/milvus"                               | tee -a /home/$USER/stop/stop_milvus.sh 
-	echo "sudo bash standalone_embed.sh stop"                  | tee -a /home/$USER/stop/stop_milvus.sh 
-	echo "cd /home/$USER"                                      | tee -a /home/$USER/stop/stop_milvus.sh 
-	echo "netstat -aunt | grep 19530"                           | tee -a /home/$USER/stop/stop_milvus.sh 
-	#
-	# Delete milvus database as also the container
-	echo "cd /home/$USER/milvus"                            > /home/$USER/start/delete_milvus_db.sh
-	echo "echo 'Will delete milvus database'"              >> /home/$USER/start/delete_milvus_db.sh 
-	echo "echo 'Data is in /home/$USER/volumes/milvus/'"   >> /home/$USER/start/delete_milvus_db.sh
-	echo "sleep 5"                                         >> /home/$USER/start/delete_milvus_db.sh
-	echo "sudo bash standalone_embed.sh delete"             >> /home/$USER/start/delete_milvus_db.sh
-	#
-	ln -sT /home/$USER/start/start_milvus.sh       /home/$USER/start_milvus.sh  
-	ln -sT /home/$USER/stop/stop_milvus.sh        /home/$USER/stop_milvus.sh  
-	ln -sT /home/$USER/start/delete_milvus_db.sh   /home/$USER/delete_milvus_db.sh  
-	#
-	echo "milvus_installed.txt" > /home/$USER/milvus_installed.txt
-	chmod +x /home/$USER/*.sh
-	chmod +x /home/$USER/start/*.sh
-	chmod +x /home/$USER/stop/*.sh
-	sleep 3
+	# echo "  "
+	# echo "   "
+	# cd /home/$USER/
+	# echo "====  "    
+	# echo "Installing milvus vector database using docker"       
+	# echo "You may be asked for the password. Supply it..."     
+	# echo "====  "                                                   
+	# sleep 3
+	# echo $password | sudo -S  apt-get update
+	# curl -sfL https://raw.githubusercontent.com/milvus-io/milvus/master/scripts/standalone_embed.sh -o standalone_embed.sh
+	# bash standalone_embed.sh start
+	# echo " "
+	# echo "Milvus vector database installed"                      
+	# echo "Ports used are: 9091 and 19530."                       
+	# echo "To restart/stop docker use the following commands:"            
+	# echo "     sudo bash standalone_embed.sh restart|start|stop|upgrade|delete"                      
+	# mkdir /home/$USER/milvus
+	# mv standalone_embed.sh /home/$USER/milvus/
+	# echo 'export PATH="$PATH:/home/$USER/milvus/"' >> /home/$USER/.bashrc
+	# # Our milvus start script		
+	# echo '#!/bin/bash'                                         | tee    /home/$USER/start/start_milvus.sh
+	# echo " "                                                   | tee -a /home/$USER/start/start_milvus.sh
+	# echo "cd ~/"                                               | tee -a /home/$USER/start/start_milvus.sh
+	# echo "echo 'Ports are: 9091 and 19530.'"                   | tee -a /home/$USER/start/start_milvus.sh
+	# echo "echo 'Data is in /home/$USER/volumes/milvus/'"                   | tee -a /home/$USER/start/start_milvus.sh
+	# echo "echo 'Access in flowise as: http://<hostIP>:19530.'"           | tee -a /home/$USER/start/start_milvus.sh
+	# echo "cd /home/$USER/milvus"                               | tee -a /home/$USER/start/start_milvus.sh
+	# echo "bash standalone_embed.sh start"                      | tee -a /home/$USER/start/start_milvus.sh
+	# echo "cd /home/$USER"                                       | tee -a /home/$USER/start/start_milvus.sh 
+	# echo "netstat -aunt | grep 19530"                          | tee -a /home/$USER/start/start_milvus.sh
+	# # Stop script		
+	# echo '#!/bin/bash'                                         | tee    /home/$USER/stop/stop_milvus.sh 
+	# echo " "                                                   | tee -a /home/$USER/stop/stop_milvus.sh 
+	# echo "cd ~/"                                               | tee -a /home/$USER/stop/stop_milvus.sh 
+	# echo "cd /home/$USER/milvus"                               | tee -a /home/$USER/stop/stop_milvus.sh 
+	# echo "sudo bash standalone_embed.sh stop"                  | tee -a /home/$USER/stop/stop_milvus.sh 
+	# echo "cd /home/$USER"                                      | tee -a /home/$USER/stop/stop_milvus.sh 
+	# echo "netstat -aunt | grep 19530"                           | tee -a /home/$USER/stop/stop_milvus.sh 
+	# #
+	# # Delete milvus database as also the container
+	# echo "cd /home/$USER/milvus"                            > /home/$USER/start/delete_milvus_db.sh
+	# echo "echo 'Will delete milvus database'"              >> /home/$USER/start/delete_milvus_db.sh 
+	# echo "echo 'Data is in /home/$USER/volumes/milvus/'"   >> /home/$USER/start/delete_milvus_db.sh
+	# echo "sleep 5"                                         >> /home/$USER/start/delete_milvus_db.sh
+	# echo "sudo bash standalone_embed.sh delete"             >> /home/$USER/start/delete_milvus_db.sh
+	# #
+	# ln -sT /home/$USER/start/start_milvus.sh       /home/$USER/start_milvus.sh  
+	# ln -sT /home/$USER/stop/stop_milvus.sh        /home/$USER/stop_milvus.sh  
+	# ln -sT /home/$USER/start/delete_milvus_db.sh   /home/$USER/delete_milvus_db.sh  
+	# #
+	# echo "milvus_installed.txt" > /home/$USER/milvus_installed.txt
+	# chmod +x /home/$USER/*.sh
+	# chmod +x /home/$USER/start/*.sh
+	# chmod +x /home/$USER/stop/*.sh
+	# sleep 3
 	
-	LINE="  9. Milvus installed"
-	if ! grep -qF "$LINE" "$FILE"; then
-	    echo "$LINE" >> "$FILE"
-	fi
+	# LINE="  9. Milvus installed"
+	# if ! grep -qF "$LINE" "$FILE"; then
+	#     echo "$LINE" >> "$FILE"
+	# fi
 		
 	echo "vectordb_installed.txt" > /home/$USER/vectordb_installed.txt
 
 	# Start all vector databases to check
-	bash stop_milvus.sh
+	#bash stop_milvus.sh
     bash start_postgresql.sh
 	bash start_chroma.sh  
 	bash start_meilisearch.sh
-	bash start_milvus.sh
+	#bash start_milvus.sh
 	echo "  "
     echo "  "
 	echo "Postgresql started?"
@@ -995,11 +995,10 @@ if [ ! -f /home/$USER/vectordb_installed.txt ]; then
 	netstat -aunt | grep 8000
 	echo "meilisearch started"
 	netstat -aunt | grep 7700
-	echo "milvus started"
-	netstat -aunt | grep 19530
+	#echo "milvus started"
+	#netstat -aunt | grep 19530
 	sleep 8
 
-	
 	echo $password | sudo -S systemctl reboot -i
 else
     echo "  "
