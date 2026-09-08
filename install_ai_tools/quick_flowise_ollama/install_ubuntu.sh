357cacc8ad9fd33752e2868b3e196d7c3438556d
harnalashok <47495816+harnalashok@users.noreply.github.com>
2026-09-08 05:58:41 +0530

    minor change

--- install_ai_tools/quick_flowise_ollama/install_ubuntu.sh
+++ install_ai_tools/quick_flowise_ollama/install_ubuntu.sh
@@ -2799,6 +2799,9 @@ if [ ! -f /home/$USER/crewaiExamples_installed.txt ]; then
 	rm -rf /home/$USER/Documents/crewaiExamples
 	cd /home/$USER
 	\cp -r /home/$USER/Documents/crewaiModels/. /home/$USER/crewai_pjt
+	mkdir /home/ashok/crewai_pjt/servers
+    cp /home/ashok/crewai_pjt/mcp_servers/'maths_stdio server'/maths_stdio_server.py /home/ashok/crewai_pjt/servers/
+    cp /home/ashok/crewai_pjt/mcp_servers/'maths_stdio server'/maths_stdio_client.py /home/ashok/crewai_pjt/
 	echo "crewaiExamples_installed.txt" > /home/$USER/crewaiExamples_installed.txt
 else
 	echo "  "
